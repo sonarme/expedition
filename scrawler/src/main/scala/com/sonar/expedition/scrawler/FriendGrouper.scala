9597aee03d135a3274f48b50f8d8fedd68c97b14
skodanga <Skbball1!>
2012-06-19 16:40:44 -0400

    hashed friendGrouper

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/FriendGrouper.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/FriendGrouper.scala
@@ -16,7 +16,7 @@ import grizzled.slf4j.Logging
 
 class FriendGrouper(args: Args) extends Job(args) {
     var inputData = "/tmp/friendData.txt"
-    var out = "/tmp/hasheduserGroupedFriends.txt"
+    var out = "/tmp/userGroupedFriends.txt"
     //   logger.debug(checkin.getUserProfileId() + "::" + checkin.getServiceType() + "::" + checkin.getServiceProfileId() + "::" + checkin.getServiceCheckinId() + "::" + checkin.getVenueName() + "::" + checkin.getVenueAddress() + "::" + checkin.getCheckinTime() + "::" + checkin.getGeohash() + "::" + checkin.getLatitude() + "::" + checkin.getLongitude() + "::" + checkin.getMessage())
     var data = (TextLine(inputData).read.project('line).map(('line) ->('userProfileId, 'serviceType, 'serviceProfileId, 'friendName)) {
         line: String => {
@@ -27,16 +27,35 @@ class FriendGrouper(args: Args) extends Job(args) {
         }
     }).pack[FriendObjects](('serviceType, 'serviceProfileId, 'friendName) -> 'friend).groupBy('userProfileId) {
         group => group.toList[FriendObjects]('friend,'friendData)
-    }.map(Fields.ALL -> ('ProfileId, 'friendName)){
+    }.map(Fields.ALL -> ('ProfileId, 'serviceType, 'serviceProfileId, 'friendName)){
         fields : (String,List[FriendObjects]) =>
             val (userid, friends)    = fields
-            val friendName = md5SumString(getFriendName(friends).getBytes("UTF-8"))
-            (userid, friendName)
-    }.project('ProfileId,'friendName).write(TextLine(out))
+            val friendName = getFriendName(friends)
+            (userid, serviceType, serviceProfileId, friendName)
+    }.project('ProfileId, 'friendName).write(TextLine(out))
+
+//    This commented section below handles the obfuscation of the userProfileID
+
+     /*.project(Fields.ALL).discard(0).map(Fields.ALL -> ('ProfileId, 'serType, 'serProfileId, 'friName)){
+        fields : (String, String, String, String) =>
+            val (userid, serviceType, serviceProfileId, friendName)    = fields
+            val hashedServiceProfileId = md5SumString(serviceProfileId.getBytes("UTF-8"))
+            (userid, serviceType, hashedServiceProfileId, friendName)
+    }.project('ProfileId, 'serType, 'serProfileId, 'friName).write(TextLine(out))*/
 
     def getFriendName(friends:List[FriendObjects]) : String ={
 
         friends.map(_.getFriendName).toString()
+    }
+
+    def getServiceType(friends:List[FriendObjects]) : String ={
+
+        friends.map(_.getServiceType).toString()
+    }
+
+    def getServiceProfileId(friends:List[FriendObjects]) : String ={
+
+        friends.map(_.getServiceProfileId).toString()
     }
 
     def md5SumString(bytes : Array[Byte]) : String = {
