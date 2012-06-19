7249a0ae9c235a418caecb960a38e7157793c4e5
zeshanwang <zeshan.wang@gmail.com>
2012-06-19 12:17:57 -0400

    friendgrouper

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/FriendGrouper.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/FriendGrouper.scala
@@ -1,35 +1,50 @@
+// package com.sonar.expedition.scrawler
+
 import cascading.tuple.Fields
 import com.sonar.dossier.domain.cassandra.converters.JsonSerializer
 import com.sonar.dossier.dto.ServiceProfileDTO
-import com.sonar.expedition.scrawler.MeetupCrawler
+import com.sonar.expedition.scrawler.FriendObjects
+
+//import com.sonar.expedition.scrawler.MeetupCrawler
 import com.twitter.scalding._
 import java.nio.ByteBuffer
-import CheckinGrouper._
+import FriendGrouper._
 import util.matching.Regex
 import grizzled.slf4j.Logging
 
 
 class FriendGrouper(args: Args) extends Job(args) {
-
-    val inputData = "/tmp/friendData.txt"
-    val out = "/tmp/userGroupedFriends.txt"
-    var data = (TextLine(inputData).read.project('line).map(('line) ->('id, 'frienddata)) {
+    var inputData = "/tmp/friendData.txt"
+    var out = "/tmp/userGroupedFriends.txt"
+    //   logger.debug(checkin.getUserProfileId() + "::" + checkin.getServiceType() + "::" + checkin.getServiceProfileId() + "::" + checkin.getServiceCheckinId() + "::" + checkin.getVenueName() + "::" + checkin.getVenueAddress() + "::" + checkin.getCheckinTime() + "::" + checkin.getGeohash() + "::" + checkin.getLatitude() + "::" + checkin.getLongitude() + "::" + checkin.getMessage())
+    var data = (TextLine(inputData).read.project('line).map(('line) ->('userProfileId, 'serviceType, 'serviceProfileId, 'friendName)) {
         line: String => {
             line match {
-                case ExtractLine(userProfileId, friendData) => (userProfileId, friendData)
-                case _ => ("1","2")
+                case DataExtractLine(id, other2, serviceID, serviceType, friendName, other) => (id, serviceType, serviceID, friendName)
+                case _ => ("None","None","None","None")
             }
         }
-    }).groupBy('id) {
+    }).pack[FriendObjects](('serviceType, 'serviceProfileId, 'friendName) -> 'friend).groupBy('userProfileId) {
+        //        var packedData = data.pack[Checkin](('userProfileId, 'serviceType, 'serviceProfileId, 'serviceCheckinID, 'venueName, 'venueAddress, 'checkinTime, 'geohash, 'latitude, 'longitude, 'message) -> 'person)
+        //        group => group.toList[Tuple8[String,String,String,String,String,String,String,String]](packedData,'iid)
+        //_.sortBy('userProfileId)
+        group => group.toList[FriendObjects]('friend,'friendData)
+    }.map(Fields.ALL -> ('ProfileId, 'friendName)){
+        fields : (String,List[FriendObjects]) =>
+            val (userid, friends)    = fields
+            val friendName = getFriendName(friends)
+            (userid, friendName)
+    }.project('ProfileId,'friendName).write(TextLine(out))
 
-        group => group.toList[String]('frienddata,'iid)
-    }.write(TextLine(out))
+    def getFriendName(friends:List[FriendObjects]) : String ={
 
+        friends.map(_.getFriendName).toString()
+    }
 }
 
 
 object FriendGrouper {
-    val ExtractLine: Regex = """([a-zA-Z\d\-]+)::(.*)""".r
+    val DataExtractLine: Regex = """([a-zA-Z\d\-]+):(.*)"id":"(.*)","service_type":"(.*)","name":"(.*)","photo(.*)""".r
 }
 
 
---
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/FriendObjects.java
@@ -0,0 +1,33 @@
+package com.sonar.expedition.scrawler;
+
+public class FriendObjects {
+    String serviceType ="";
+    String serviceProfileID="";
+    String friendName="";
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getServiceProfileId() {
+        return serviceProfileID;
+    }
+
+    public void setServiceProfileId(String serviceProfileId) {
+        this.serviceProfileID = serviceProfileId;
+    }
+
+
+    public String getFriendName() {
+        return friendName;
+    }
+
+    public void setFriendName(String venueName) {
+        this.friendName = venueName;
+    }
+
+}
