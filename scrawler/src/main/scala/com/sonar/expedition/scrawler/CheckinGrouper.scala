dff8dad19d91d1fbba8f84cd41f817951a1431d8
skodanga <Skbball1!>
2012-06-19 18:34:09 -0400

    checkingrouper cleanup

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/CheckinGrouper.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/CheckinGrouper.scala
@@ -4,6 +4,7 @@ import cascading.tuple.Fields
 import com.sonar.dossier.domain.cassandra.converters.JsonSerializer
 import com.sonar.dossier.dto.ServiceProfileDTO
 import com.sonar.expedition.scrawler.CheckinObjects
+import java.security.MessageDigest
 
 //import com.sonar.expedition.scrawler.MeetupCrawler
 import com.twitter.scalding._
@@ -18,33 +19,47 @@ import com.sonar.dossier.dto.{Checkin, ServiceProfileDTO}
 class CheckinGrouper(args: Args) extends Job(args) {
 
     var inputData = "/tmp/checkinData.txt"
-    var out = "/tmp/userGroupedCheckins.txt"
-    var data = (TextLine(inputData).read.project('line).map(('line) ->('userProfileId, 'serviceType, 'serviceProfileId, 'serviceCheckinID, 'venueName, 'venueAddress, 'checkinTime, 'geohash, 'latitude, 'longitude, 'message)) {
+    var out = "/tmp/hasheduserGroupedCheckins.txt"
+    var data = (TextLine(inputData).read.project('line).map(('line) ->('userProfileID, 'serviceType, 'serviceProfileID, 'serviceCheckinID, 'venueName, 'venueAddress, 'checkinTime, 'geohash, 'latitude, 'longitude)) {
         line: String => {
             line match {
-                case DataExtractLine(id, serviceType, serviceID, serviceCheckinID, venueName, venueAddress, checkinTime, geoHash, lat, lng, message) => (id, serviceType, serviceID, serviceCheckinID, venueName, venueAddress, checkinTime, geoHash, lat, lng, message)
-                case _ => ("None","None","None","None","None","None","None","None","None","None","None")
+                case DataExtractLine(id, serviceType, serviceID, serviceCheckinID, venueName, venueAddress, checkinTime, geoHash, lat, lng) => (id, serviceType, serviceID, serviceCheckinID, venueName, venueAddress, checkinTime, geoHash, lat, lng)
+                case _ => ("None","None","None","None","None","None","None","None","None","None")
             }
         }
-    }).pack[CheckinObjects](('serviceType, 'serviceProfileId, 'serviceCheckinID, 'venueName, 'venueAddress, 'checkinTime, 'geohash, 'latitude, 'longitude, 'message) -> 'checkin).groupBy('userProfileId) {
+    })/*.pack[CheckinObjects](('serviceType, 'serviceProfileID, 'serviceCheckinID, 'venueName, 'venueAddress, 'checkinTime, 'geohash, 'latitude, 'longitude, 'message) -> 'checkin).groupBy('userProfileID) {
 
         group => group.toList[CheckinObjects]('checkin,'checkindata)
-    }.map(Fields.ALL -> ('ProfileId, 'venue)){
+    }.map(Fields.ALL -> ('ProfileID, 'venue)){
         fields : (String,List[CheckinObjects]) =>
         val (userid ,checkin) = fields
         val venue = getVenue(checkin)
         (userid,venue)
-    }.project('ProfileId,'venue).write(TextLine(out))
+    }.project('ProfileID,'venue).write(TextLine(out))*/
+
+     .project(Fields.ALL).discard(0).map(Fields.ALL -> ('ProfileID, 'serType, 'serProfileID, 'serCheckinID, 'venName, 'venAddress, 'chknTime, 'ghash, 'lat, 'lng)){
+        fields : (String, String, String, String, String, String, String, String, String, String) =>
+            val (id, serviceType, serviceID, serviceCheckinID, venueName, venueAddress, checkinTime, geoHash, lat, lng)    = fields
+            val hashedServiceID = md5SumString(serviceID.getBytes("UTF-8"))
+            (id, serviceType, hashedServiceID, serviceCheckinID, venueName, venueAddress, checkinTime, geoHash, lat, lng)
+    }.project('ProfileID, 'serType, 'serProfileID, 'serCheckinID, 'venName, 'venAddress, 'chknTime, 'ghash, 'lat, 'lng).write(TextLine(out))
 
     def getVenue(checkins:List[CheckinObjects]) : String ={
 
         checkins.map(_.getVenueName).toString
     }
 
+    def md5SumString(bytes : Array[Byte]) : String = {
+        val md5 = MessageDigest.getInstance("MD5")
+        md5.reset()
+        md5.update(bytes)
+        md5.digest().map(0xFF & _).map { "%02x".format(_) }.foldLeft(""){_ + _}
+    }
+
 }
 
 object CheckinGrouper {
-    val DataExtractLine: Regex = """([a-zA-Z\d\-]+)::(.*)::(.*)::(.*)::(.*)::(.*)::(.*)::(.*)::(.*)::(.*)::(.*)""".r
+    val DataExtractLine: Regex = """([a-zA-Z\d\-]+)::(.*)::(.*)::(.*)::(.*)::(.*)::(.*)::(.*)::(.*)::(.*)""".r
 }
 
 
