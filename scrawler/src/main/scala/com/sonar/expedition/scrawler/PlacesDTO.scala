86048c747ea1e7c500ac6cc5967a8610c998de02
skodanga <Skbball1!>
2012-07-03 17:53:15 -0400

    DTOProfileInfoPush cleanup

---
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/PlacesDTO.scala
@@ -0,0 +1,20 @@
+package com.sonar.expedition.scrawler
+
+import reflect.BeanProperty
+import annotation.target.field
+import org.codehaus.jackson.map.annotate.JsonDeserialize
+import ch.hsr.geohash.GeoHash
+import com.sonar.dossier.service.PrecomputationSettings
+
+case class PlacesDTO(@BeanProperty
+                     var geometry: PlacesGeometryDTO,
+                     @BeanProperty
+                     var `type`: String,
+                     @BeanProperty
+                     var id: String,
+                     @BeanProperty
+                     var properties: PlacesPropertiesDTO) {
+
+//    def this() = this(0, 0)
+
+}
