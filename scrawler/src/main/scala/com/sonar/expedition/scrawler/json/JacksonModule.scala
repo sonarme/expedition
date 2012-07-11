1da76c53fa08fb3dcdb1b200dbed26b41642587c
Paul Fisher <paul@k2media.net>
2012-07-11 17:56:54 -0400

    switched to 2.0.0 version of jackson and implemented a scala module

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/json/JacksonModule.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/json/JacksonModule.scala
@@ -8,8 +8,9 @@ import com.fasterxml.jackson.databind._
 import com.sonar.dossier.dto.{Checkin, UserEducation, UserEmployment, ServiceProfileDTO}
 import deser.DeserializationProblemHandler
 import grizzled.slf4j.Logging
+import org.codehaus.jackson.annotate.JsonProperty
 
-class ScrawlerModule extends SimpleModule("scrawler", new Version(1, 0, 1, null)) {
+class ScrawlerModule extends SimpleModule("scrawler", new Version(1, 0, 0, null, "com.sonar", "scrawler")) {
     override def setupModule(context: SetupContext) {
         context.addDeserializationProblemHandler(new DeserializationProblemHandlerImpl)
     }
@@ -25,29 +26,55 @@ class DeserializationProblemHandlerImpl extends DeserializationProblemHandler wi
 
 class ScrawlerObjectMapper extends ObjectMapper {
 
-    setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES)
+    this.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES)
     this.getDeserializationConfig.without(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
     this.getSerializationConfig.without(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+    this.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
 }
 
 object ScrawlerObjectMapper {
 
+    var initialized = false
+    val module = new ScrawlerModule()
     val objectMapper = new ScrawlerObjectMapper()
-    val module = new ScrawlerModule();
-    // functionality includes ability to register serializers, deserializers, add mix-in annotations etc:
-    module.setMixInAnnotation(classOf[ServiceProfileDTO], classOf[IgnoreUnknownMixin])
-    module.setMixInAnnotation(classOf[UserEmployment], classOf[IgnoreUnknownMixin])
-    module.setMixInAnnotation(classOf[UserEducation], classOf[IgnoreUnknownMixin])
-    module.setMixInAnnotation(classOf[Checkin], classOf[IgnoreUnknownMixin])
-    // and the magic happens here when we register module with mapper:
-    objectMapper.registerModule(module);
+
+    init()
+
+    def init() {
+        // functionality includes ability to register serializers, deserializers, add mix-in annotations etc:
+        module.setMixInAnnotation(classOf[ServiceProfileDTO], classOf[IgnoreUnknownMixin])
+        module.setMixInAnnotation(classOf[UserEducation], classOf[IgnoreUnknownMixin])
+        module.setMixInAnnotation(classOf[Checkin], classOf[IgnoreUnknownMixin])
+        module.setMixInAnnotation(classOf[UserEmployment], classOf[UserEmploymentMixin])
+        // and the magic happens here when we register module with mapper:
+        objectMapper.registerModule(module)
+        initialized = true
+    }
+
+
 
     def mapper(): ObjectMapper = {
+        if (!initialized) {
+            init()
+        }
         objectMapper
     }
 
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-abstract class IgnoreUnknownMixin
+abstract class IgnoreUnknownMixin {
+
+
+
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+abstract class UserEmploymentMixin {
+
+    @scala.reflect.BeanProperty
+    @JsonProperty("is_current")
+    var current:Boolean = _
+
+}
 
