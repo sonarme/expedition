1b7032f8fbacbd2dbabe5cc4f82c3e0b127bbf76
jyotirmoy sundi <sundi133@gmail.com>
2012-07-17 12:00:41 -0400

    mahout

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/MahoutClassification.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/MahoutClassification.scala
@@ -3,7 +3,10 @@ package com.sonar.expedition.scrawler.jobs
 
 import com.twitter.scalding.{TextLine, Job, Args}
 import com.sonar.expedition.scrawler.pipes.JobCodeReader._
-
+import scala.Predef._
+import com.twitter.scalding.TextLine
+import scala._
+import com.twitter.scalding.TextLine
 
 
 class MahoutClassification(args: Args) extends Job(args) {
@@ -46,17 +49,27 @@ class MahoutClassification(args: Args) extends Job(args) {
         _
                 .toList[String]('matrixocctitle3,'matrixocctitle4)
                 .toList[Int]('size,'size1)
-    }.mapTo(('cpsocctite1,'matrixocctitle4,'size1)->('cpsocctite2,'matrixocctitle5,'size2)){
+    }.mapTo(('cpsocctite1,'matrixocctitle4,'size1)->('cpsocctite2,'matrixocctitle5,'rms)){
         fields: (String,List[String],List[Int]) =>
         val (job,words,freqlist )= fields
         val freq=normaliseList(freqlist)
 
         (job,words,freq )
     }
+    .mapTo(('cpsocctite2,'matrixocctitle5,'rms)->('cpsocctite2,'matrixocctitlerms)){
+            fields: (String,List[String],List[Double]) =>
+            val (job,words,freqlist )= fields
+            val words_rms=combineList(words,freqlist)
+            (job,words_rms)
+    }.flatMapTo(('cpsocctite2,'matrixocctitlerms)->('cpsocctite2,'matrixocctitle , 'rms)){
+            fields: (String,List[String]) =>
+            val (job,words_rms)= fields
+            words_rms.map{word:String => val wrds=word.split("::"); (job,wrds(0),wrds(1))}
+    }
     .write(TextLine("/tmp/occupationCodetsvtest2.txt"))
 
      val docfreq = pipe.flatMapTo(('cpsocctite,'matrixocctitle2)->('cpsocctite1,'matrixocctitle3)){
-         fields: (String,List[String]) =>
+             fields: (String,List[String]) =>
              val (job,words )= fields
              words.map{word:String => (job,word)}
 
@@ -70,16 +83,44 @@ class MahoutClassification(args: Args) extends Job(args) {
          fields: (String,List[String]) =>
          val (words,jobcategory )= fields
          (words,jobcategory.size)
-     }
-     .project(('matrixocctitle4,'cpsocctite3))
+     }.project('matrixocctitle4,'cpsocctite3)
+     /*.groupAll{
+         _
+            .toList[String]('matrixocctitle4,'matrixocctitle5)
+            .toList[Int]('cpsocctite3,'cpsocctite4)
+
+
+     }.mapTo(('matrixocctitle5,'cpsocctite4)->('matrixocctitle6,'cpsocctite5,'cpsocctite2,'matrixocctitle5,'rms)) {
+         fields: (List[String],List[Int]) =>
+         val (terms,docfreq )= fields
+         termfreq.map{
+             fields: (String,List[String],List[Double]) =>
+             val (job,words,freqlist )= fields
+             (job,words,freqlist,terms,docfreq)
+         }
+
+     }*/
+     //.project(('matrixocctitle4,'cpsocctite3))
      //.project(('matrixocctitle3,'cpsocctite2))
      .write(TextLine("/tmp/occupationCodetsvtest3.txt"))
 
-      def normaliseList(freq:List[Int])={
+      def normaliseList(freq:List[Int]):List[Double]={
           val dividend = math.sqrt(freq reduce {(acc,elem)=> acc+(elem*elem)})
           freq map (_ / dividend)
 
       }
+
+    def combineList(words:List[String],rms:List[Double]) :List[String] = {
+        var dm  = List[String]()
+        //var dk = List[Map[String,Double]]()
+
+        for (i <- 0 until words.size){
+            dm = words(i) +"::"+ rms(i) :: dm
+           // dk = Map(words(i) -> rms(i)) :: dk
+        }
+        //println(dm(0))
+        dm
+    }
 
 }
 
