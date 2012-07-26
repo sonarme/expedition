368bf4bf598531ca52d3f66ea1c1a3d3751b93a5
jyotirmoy sundi <sundi133@gmail.com>
2012-07-26 16:18:53 -0400

    meetup code refactoring

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/meetup/MeetUpCrawl.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/meetup/MeetUpCrawl.scala
@@ -19,40 +19,41 @@ output : the file to which the non visited profile links will be written to
 
  */
 class MeetUpCrawl(args: Args) extends Job(args) {
-    val input = TextLine(args("input"))
+    //val input = TextLine(args("input"))
     //read the inout file , the inout file is created after donwlaod the files manually from sitemap.xml and unziping them , finaaly a combined file is prepared out of all the unzippied files, these combined file contains all the urls in 1st and 2nd level of depth obtained from sitemap.xml
 
-    var links1 =
-        (
-                (
-                        //TextLine(args.apply("input"))
-                        input.read
-                                .project('line)
-                                .filter('line) {
-                            text: String => text.contains("loc>http:")
-                        }.mapTo('line -> 'tmplinks) {
-                            line: String => (line.trim)
-                        }.mapTo('tmplinks -> 'links) {
-                            tmplinks: String => (tmplinks.substring(5, tmplinks.indexOf("</loc>")))
-                        }
-                                .flatMapTo('links -> 'memberlinks) {
+    /* var links1 =
+(
+(
+    //TextLine(args.apply("input"))
+    input.read
+            .project('line)
+            .filter('line) {
+        text: String => text.contains("loc>http:")
+    }.mapTo('line -> 'tmplinks) {
+        line: String => (line.trim)
+    }.mapTo('tmplinks -> 'links) {
+        tmplinks: String => (tmplinks.substring(5, tmplinks.indexOf("</loc>")))
+    }
+            .flatMapTo('links -> 'memberlinks) {
 
-                            links: String =>
-                                println(links);
-                                ScrawlerUtils.extractContentsPageLinks(links) //will contain bth profile group info and pagination links , if pages more then one, add first page as a pagination link
-                        } //.write(output5)
-                        )
+        links: String =>
+            println(links);
+            ScrawlerUtils.extractContentsPageLinks(links) //will contain bth profile group info and pagination links , if pages more then one, add first page as a pagination link
+    } //.write(output5)
+    )*/
 
 
-                //start of sitemap
-                /*  var links1 = ((TextLine(args.apply("input")).read
-                        .project('line)
-                        .flatMap('line -> 'links) {
-                    line: String => MeetupCrawler.importLinks(line).split("\\n")
-                }.flatMapTo('links -> 'memberlinks) {
-                    links: String => ScrawlerUtils.extractContentsPageLinks(links) //will contain  pagination links , if pages more then one, add first page as a pagination link
-                })*/
-                ).write(TextLine("/tmp/meet2"))
+    //start of sitemap
+    var links1 = ((TextLine(args.apply("input")).read
+            .project('line)
+            .flatMap('line -> 'links) {
+        line: String => MeetupCrawler.importLinks(line).split("\\n")
+    }.flatMapTo('links -> 'memberlinks) {
+        links: String => ScrawlerUtils.extractContentsPageLinks(links) //will contain  pagination links , if pages more then one, add first page as a pagination link
+    })
+            )
+    //).write(TextLine("/tmp/meet2"))
     //end of sitemap
 
     //profiles from paginations links
