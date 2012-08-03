1447d39d64f87b678ea0fd5aafab0fe5d882c6b3
zeshanwang <zeshan.wang@gmail.com>
2012-08-03 14:28:44 -0400

    sonar checkin venue

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/pipes/SonarCheckinVenue.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/pipes/SonarCheckinVenue.scala
@@ -1,6 +1,7 @@
 package com.sonar.expedition.scrawler.pipes
 
 import com.twitter.scalding._
+import com.sonar.expedition.scrawler.util._
 
 class SonarCheckinVenue(args: Args) extends Job(args) {
 
@@ -8,23 +9,79 @@ class SonarCheckinVenue(args: Args) extends Job(args) {
     // ('keyid, 'serType, 'serProfileID, 'serCheckinID, 'venName, 'venAddress, 'chknTime, 'ghash, 'loc, 'dayOfYear, 'dayOfWeek, 'hour)
     def getCheckinVenue(checkins: RichPipe): RichPipe = {
 
+        val default = ("", "", 0.0, 0.0, 0)
+        val cutoff = 3
+        val havver = new Haversine
+
         val sonarPipe = checkins
                 .filter('serType) {
             serType: String => serType.equals("sonar")
         }
-                .project(('keyid, 'loc))
+                .unique(('keyid, 'loc))
                 .rename(('keyid, 'loc) -> ('keyid2, 'loc2))
 
-        val joinedPipe = checkins.joinWithSmaller(('keyid -> 'keyid2), sonarPipe)
-                .groupBy(('keyid, 'loc2)) {
-            _.toList[(String, String)](('venName, 'loc) -> 'checkinList)
+        val namePipe = checkins
+                .filter('serType) {
+            serType: String => !serType.equals("sonar")
+        }
+//                .filter('venName) {
+//            venName: String => !venName.equals("")
+//        }
+                .groupBy(('keyid, 'venName, 'loc)) {
+            _.size('count)
         }
 
-//        val output = joinedPipe.flatMap(('loc2, 'checkinList) -> ('venName, 'loc, 'score)) {
-//
-//        }
+        val joinedPipe = namePipe.joinWithSmaller(('keyid -> 'keyid2), sonarPipe)
+                .groupBy(('keyid, 'loc2)) {
+            _.toList[(String, String, String)](('venName, 'loc, 'count) -> 'checkinList)
+        }
 
-        joinedPipe
+        val output = joinedPipe.flatMap('checkinList -> ('venName, 'loc, 'count)) {
+                list: List[(String, String, String)] => list
+        }
+                .discard('checkinList)
+                .map(('loc2, 'loc) -> 'score) {
+            fields: (String, String) => {
+                val (sonarLoc, loc) = fields
+                val sonarLat = sonarLoc.split(":").head.toDouble
+                val sonarLng = sonarLoc.split(":").last.toDouble
+                val lat = loc.split(":").head.toDouble
+                val lng = loc.split(":").last.toDouble
+                val dist = havver.haversine(sonarLat, sonarLng, lat, lng)
+
+                dist
+            }
+        }
+                .filter('score) {
+            score: Double => score < cutoff
+        }
+                .map(('score, 'count) -> 'adjustedScore) {
+            fields: (Double, Int) => {
+                val (score, count) = fields
+                -score / scala.math.sqrt(count)
+            }
+        }
+                .groupBy(('keyid, 'loc2)) {
+            _.toList[(String, String, Double, Double, Int)](('loc, 'venName, 'score, 'adjustedScore, 'count) -> 'infoList).sortBy('adjustedScore)
+        }
+                .map('infoList -> 'topTwo) {
+            list: List[(String, String, Double, Double, Int)] => {
+                val newList = List[(String, String, Double, Double, Int)]()
+                newList.::(list.tail.headOption.getOrElse(default)).::(list.head)
+            }
+        }
+                .discard('infoList)
+                .flatMap('topTwo -> ('loc, 'venName, 'score, 'adjustedScore, 'count)) {
+            list: List[(String, String, Double, Double, Int)] => {
+                list
+            }
+        }
+                .discard(('topTwo, 'score))
+                .filter('loc) {
+            loc: String => !loc.isEmpty
+        }
+
+        output
 
     }
 
