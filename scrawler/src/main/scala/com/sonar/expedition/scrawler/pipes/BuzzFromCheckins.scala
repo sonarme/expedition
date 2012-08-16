2d9306acc296433a4bbb6d11bc1ce6e7e1b7beb8
Shyam Kodangattil <shyam@sonar.me>
2012-08-16 16:11:32 -0400

    buzzScore on a 1-99 scale

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/pipes/BuzzFromCheckins.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/pipes/BuzzFromCheckins.scala
@@ -2,15 +2,14 @@ package com.sonar.expedition.scrawler.pipes
 
 import com.twitter.scalding.{Job, Args, RichPipe}
 import com.sonar.expedition.scrawler.util.{StemAndMetaphoneEmployer, CommonFunctions, ShingleTokenizer}
-import org.apache.commons.lang.StringUtils
 import cascading.pipe.joiner.LeftJoin
+import cascading.tuple.Fields
 
 class BuzzFromCheckins(args: Args) extends Job(args) {
 
 
     def getShingles(checkinsWithMessage: RichPipe): RichPipe = {
         val messagePipe = checkinsWithMessage
-        //        val withoutMessagePipe = checkinsWithoutMessage   , checkinsWithoutMessage: RichPipe
 
         val shinglePipe = messagePipe
                 .filter('msg) {
@@ -42,6 +41,11 @@ class BuzzFromCheckins(args: Args) extends Job(args) {
                 val stemmedVenName = StemAndMetaphoneEmployer.getStemmed(venName)
                 (stemmedVenName)
         }
+                .filter('stemmedVenName) {
+            fields: (String) =>
+                val venName = fields
+                (!CommonFunctions.isNullOrEmpty(venName))
+        }
                 .joinWithSmaller('stemmedVenName -> 'singleShingle, shingles, joiner = new LeftJoin)
                 .groupBy('stemmedVenName) {
             _
@@ -54,6 +58,67 @@ class BuzzFromCheckins(args: Args) extends Job(args) {
                 .project('stemmedVenName, 'singleShinglesize)
         buzz
     }
+
+    def findBuzzStats(buzz: RichPipe): RichPipe = {
+        val buzzStats = buzz
+                //                .filter('singleShinglesize) {
+                //            fields: (String) =>
+                //                val (size) = fields
+                //                (size.toInt > 1)
+                //        }
+                .groupAll {
+            _
+                    .sizeAveStdev('singleShinglesize ->('size, 'avg, 'stdev))
+        }
+        buzzStats
+    }
+
+    def findMin(buzz: RichPipe): RichPipe = {
+        val min = buzz
+                .groupAll {
+            _
+                    .min('normalized -> 'min)
+        }.project('min)
+        min
+    }
+
+    def findMax(buzz: RichPipe): RichPipe = {
+        val max = buzz
+                .groupAll {
+            _
+                    .max('normalized -> 'max)
+        }.project('max)
+        max
+    }
+
+    def normalizedBuzz(buzz: RichPipe, buzzStats: RichPipe): RichPipe = {
+        val normalizedBuzz = buzz
+                .crossWithTiny(buzzStats)
+                .map(('singleShinglesize, 'avg) -> ('normalized)) {
+            fields: (String, String) =>
+                val (buzz, avg) = fields
+                val normalized = buzz.toInt / avg.toDouble
+                val log = scala.math.log(normalized)
+                log
+        }
+                .project('stemmedVenName, 'normalized)
+        normalizedBuzz
+
+    }
+
+    def buzzScore(normalizedBuzz: RichPipe, min: RichPipe, max: RichPipe): RichPipe = {
+        val buzzScore = normalizedBuzz
+                .crossWithTiny(min)
+                .crossWithTiny(max)
+                .map(('normalized, 'min, 'max) -> 'buzzScore) {
+            fields: (Double, Double, Double) =>
+                val (buzz, min, max) = fields
+                var score = 0.0
+                score = (buzz + (-min)) * (98 / (max + (-(min))))
+                (score + 1.0)
+        }
+        buzzScore
+    }.project('stemmedVenName, 'buzzScore)
 
 
 }
--- scrawler/src/main/scala/com/sonar/expedition/scrawler/test/BuzzFromCheckinsTest.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/test/BuzzFromCheckinsTest.scala
@@ -1,38 +1,24 @@
 package com.sonar.expedition.scrawler.test
 
-import com.twitter.scalding.{RichPipe, TextLine, Job, Args}
-import com.sonar.expedition.scrawler.pipes.{PlacesCorrelation, CheckinGrouperFunction, BuzzFromCheckins}
-import cascading.tuple.Fields
+import com.twitter.scalding.{TextLine, Job, Args}
+import com.sonar.expedition.scrawler.pipes.{CheckinGrouperFunction, BuzzFromCheckins}
 
 class BuzzFromCheckinsTest(args: Args) extends Job(args) {
 
     val checkinsWithMessage = args("checkinsWithMessage")
     val checkinsWithoutMessage = args("checkinsWithoutMessage")
 
-
     val checkinGrouperPipe = new CheckinGrouperFunction(args)
     val buzzPipe = new BuzzFromCheckins(args)
 
     val msgCheckins = checkinGrouperPipe.checkinsWithMessage(TextLine(checkinsWithMessage).read)
     val noMsgCheckins = checkinGrouperPipe.unfilteredCheckinsLatLon(TextLine(checkinsWithoutMessage).read)
-    val shinglesPipe = buzzPipe.getShingles(msgCheckins)
-    val buzz = buzzPipe.findBuzz(shinglesPipe, noMsgCheckins)
-//            .map('singleShinglesize -> ('singleShinglesize, 'size)) {
-//        fields: (String) =>
-//            val (size1) = fields
-//            val size2 = size1
-//            (size1, size2)
-//    }
-//            .groupBy('singleShinglesize) {
-//        _
-//                .average('singleShinglesize -> 'avg)
-//    }.map(('singleShinglesize, 'avg) -> 'normalized) {
-//        fields: (String, String) =>
-//            val (shingleSize, avg) = fields
-//            val normalized = shingleSize.toDouble / avg.toDouble
-//            normalized
-//    }
-            .project(Fields.ALL)
-            .write(TextLine("/tmp/buzz.txt"))
+    val shinglesPipe = buzzPipe.getShingles(msgCheckins).write(TextLine("/tmp/shingles.txt"))
+    val buzz = buzzPipe.findBuzz(shinglesPipe, noMsgCheckins).write(TextLine("/tmp/buzz.txt"))
+    val buzzStats = buzzPipe.findBuzzStats(buzz).write(TextLine("/tmp/buzzStats.txt"))
+    val normalizedBuzz = buzzPipe.normalizedBuzz(buzz, buzzStats).write(TextLine("/tmp/nomrmalbuzz.txt"))
+    val buzzMin = buzzPipe.findMin(normalizedBuzz).write(TextLine("/tmp/buzzMin.txt"))
+    val buzzMax = buzzPipe.findMax(normalizedBuzz).write(TextLine("/tmp/buzzMax.txt"))
+    val buzzScore = buzzPipe.buzzScore(normalizedBuzz, buzzMin, buzzMax).write(TextLine("/tmp/buzzScore.txt"))
 
 }
