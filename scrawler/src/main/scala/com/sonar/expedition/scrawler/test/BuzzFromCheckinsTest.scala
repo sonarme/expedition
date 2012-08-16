d807dc4701b86facca32d31d1a5e2c55e2740dfb
Shyam Kodangattil <shyam@sonar.me>
2012-08-16 01:32:06 -0400

    buzz fixes

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/test/BuzzFromCheckinsTest.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/test/BuzzFromCheckinsTest.scala
@@ -4,7 +4,7 @@ import com.twitter.scalding.{RichPipe, TextLine, Job, Args}
 import com.sonar.expedition.scrawler.pipes.{PlacesCorrelation, CheckinGrouperFunction, BuzzFromCheckins}
 import cascading.tuple.Fields
 
-class BuzzFromCheckinsTest (args: Args) extends Job(args) {
+class BuzzFromCheckinsTest(args: Args) extends Job(args) {
 
     val checkinsWithMessage = args("checkinsWithMessage")
     val checkinsWithoutMessage = args("checkinsWithoutMessage")
@@ -16,7 +16,23 @@ class BuzzFromCheckinsTest (args: Args) extends Job(args) {
     val msgCheckins = checkinGrouperPipe.checkinsWithMessage(TextLine(checkinsWithMessage).read)
     val noMsgCheckins = checkinGrouperPipe.unfilteredCheckinsLatLon(TextLine(checkinsWithoutMessage).read)
     val shinglesPipe = buzzPipe.getShingles(msgCheckins)
-    val buzz = buzzPipe.findBuzz(shinglesPipe, noMsgCheckins).project(Fields.ALL)
-    .write(TextLine("/tmp/buzz.txt"))
+    val buzz = buzzPipe.findBuzz(shinglesPipe, noMsgCheckins)
+//            .map('singleShinglesize -> ('singleShinglesize, 'size)) {
+//        fields: (String) =>
+//            val (size1) = fields
+//            val size2 = size1
+//            (size1, size2)
+//    }
+//            .groupBy('singleShinglesize) {
+//        _
+//                .average('singleShinglesize -> 'avg)
+//    }.map(('singleShinglesize, 'avg) -> 'normalized) {
+//        fields: (String, String) =>
+//            val (shingleSize, avg) = fields
+//            val normalized = shingleSize.toDouble / avg.toDouble
+//            normalized
+//    }
+            .project(Fields.ALL)
+            .write(TextLine("/tmp/buzz.txt"))
 
 }
