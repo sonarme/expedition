3a277947b7717b5a3e8bacdc2d515237506e56f6
Shyam Kodangattil <shyam@sonar.me>
2012-08-16 19:33:21 -0400

    buzzScore all args

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/test/BuzzFromCheckinsTest.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/test/BuzzFromCheckinsTest.scala
@@ -8,22 +8,22 @@ class BuzzFromCheckinsTest(args: Args) extends Job(args) {
     val checkinsWithMessage = args("checkinsWithMessage")
     val checkinsWithoutMessage = args("checkinsWithoutMessage")
     val checkinsWithVenueId = args("checkinsWithVenueId")
+    val output = args("output")
 
     val checkinGrouperPipe = new CheckinGrouperFunction(args)
     val buzzPipe = new BuzzFromCheckins(args)
     val venueIdPipe = new PlacesCorrelation(args)
 
-
     val venueId = checkinGrouperPipe.correlationCheckins(TextLine(checkinsWithVenueId).read)
     val msgCheckins = checkinGrouperPipe.checkinsWithMessage(TextLine(checkinsWithMessage).read)
     val noMsgCheckins = checkinGrouperPipe.unfilteredCheckinsLatLon(TextLine(checkinsWithoutMessage).read)
-    val venueIdCheckins = venueIdPipe.withGoldenId(noMsgCheckins, venueId).write(TextLine("/tmp/venueId.txt"))
-    val shinglesPipe = buzzPipe.getShingles(msgCheckins).write(TextLine("/tmp/shingles.txt"))
-    val buzz = buzzPipe.findBuzz(shinglesPipe, venueIdCheckins).write(TextLine("/tmp/buzz.txt"))
-    val buzzStats = buzzPipe.findBuzzStats(buzz).write(TextLine("/tmp/buzzStats.txt"))
-    val normalizedBuzz = buzzPipe.normalizedBuzz(buzz, buzzStats).write(TextLine("/tmp/nomrmalbuzz.txt"))
-    val buzzMin = buzzPipe.findMin(normalizedBuzz).write(TextLine("/tmp/buzzMin.txt"))
-    val buzzMax = buzzPipe.findMax(normalizedBuzz).write(TextLine("/tmp/buzzMax.txt"))
-    val buzzScore = buzzPipe.buzzScore(normalizedBuzz, buzzMin, buzzMax).write(TextLine("/tmp/buzzScore.txt"))
+    val venueIdCheckins = venueIdPipe.withGoldenId(noMsgCheckins, venueId)
+    val shinglesPipe = buzzPipe.getShingles(msgCheckins)
+    val buzz = buzzPipe.findBuzz(shinglesPipe, venueIdCheckins)
+    val buzzStats = buzzPipe.findBuzzStats(buzz)
+    val normalizedBuzz = buzzPipe.normalizedBuzz(buzz, buzzStats)
+    val buzzMin = buzzPipe.findMin(normalizedBuzz)
+    val buzzMax = buzzPipe.findMax(normalizedBuzz)
+    val buzzScore = buzzPipe.buzzScore(normalizedBuzz, buzzMin, buzzMax).write(TextLine(output))
 
 }
