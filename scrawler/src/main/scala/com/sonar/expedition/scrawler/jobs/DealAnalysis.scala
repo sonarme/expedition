7533d3f0d34ab7d91510945b0a377e877daf67ee
Ben Kempe <ben@k2medialabs.com>
2012-08-27 18:47:06 -0400

    reading deal data

---
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/DealAnalysis.scala
@@ -0,0 +1,41 @@
+package com.sonar.expedition.scrawler.jobs
+
+import com.twitter.scalding.{Job, TextLine, Tsv, Args}
+import org.codehaus.jackson.map.ObjectMapper
+import org.codehaus.jackson.`type`.TypeReference
+import java.util
+import com.sonar.expedition.scrawler.util.StemAndMetaphoneEmployer
+import ch.hsr.geohash.GeoHash
+import com.sonar.expedition.scrawler.pipes.PlacesCorrelation.PlaceCorrelationSectorSize
+import DealAnalysis._
+import reflect.BeanProperty
+
+class DealAnalysis(args: Args) extends Job(args) {
+    val dealsInput = args("dealsInput")
+
+    Tsv(dealsInput, ('dealId, 'successfulDeal, 'merchantName, 'majorCategory, 'minorCategory, 'minPricepoint, 'locationJSON)).map(('merchantName, 'locationJSON) ->('stemmedMerchantName, 'lat, 'lng, 'geosector)) {
+        in: (String, String) =>
+            val (merchantName, locationJSON) = in
+            val dealLocations = DealObjectMapper.readValue[util.List[DealLocation]](locationJSON, new TypeReference[util.List[DealLocation]] {})
+            val dealLocation = dealLocations.head
+            val stemmedMerchantName = StemAndMetaphoneEmployer.getStemmed(merchantName)
+            val geohash = GeoHash.withBitPrecision(dealLocation.latitude, dealLocation.longitude, PlaceCorrelationSectorSize)
+            (stemmedMerchantName, dealLocation.latitude, dealLocation.longitude, geohash.longValue())
+    }.write(TextLine("dealtest"))
+}
+
+object DealAnalysis {
+    val DealObjectMapper = new ObjectMapper
+}
+
+case class DealLocation(
+                               @BeanProperty address: String,
+                               @BeanProperty city: String = null,
+                               @BeanProperty state: String = null,
+                               @BeanProperty zip: String = null,
+                               @BeanProperty phone: String = null,
+                               @BeanProperty latitude: Double = 0,
+                               @BeanProperty longitude: Double = 0
+                               ) {
+    def this() = this(null)
+}
