216febb2cf677ce4166fddfaf9a7f1b0c693ad78
Ben Kempe <ben@k2medialabs.com>
2012-08-27 23:15:39 -0400

    cleanup

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/DealAnalysis.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/DealAnalysis.scala
@@ -19,12 +19,99 @@ import com.sonar.scalding.cassandra.CassandraSource
 import com.twitter.scalding.Tsv
 import com.sonar.scalding.cassandra.NarrowRowScheme
 import cascading.tuple.Fields
+import com.sonar.expedition.scrawler.util.CommonFunctions._
+import com.sonar.expedition.scrawler.jobs.DealLocation
+import com.twitter.scalding.SequenceFile
+import scala.Some
+import com.sonar.scalding.cassandra.CassandraSource
+import com.twitter.scalding.Tsv
+import com.twitter.scalding.TextLine
+import com.sonar.scalding.cassandra.NarrowRowScheme
+import util.Date
+import java.text.SimpleDateFormat
+import cascading.pipe.Pipe
 
 class DealAnalysis(args: Args) extends Job(args) with PlacesCorrelation with CheckinGrouperFunction {
-    val rpcHostArg = args("rpcHost")
+    val rpcHostArg = args.optional("rpcHost")
     val ppmap = args.getOrElse("ppmap", "")
     val dealsInput = args("dealsInput")
     val dealsOutput = args("dealsOutput")
+    val checkinsInputArg = args.optional("checkinsInput")
+
+    val checkins: Pipe = checkinsInputArg match {
+        case Some(checkinsInput) =>
+            def getDate(chknTime: String) = {
+                val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'")
+                try {
+                    val chekingTime = chknTime.substring(chknTime.lastIndexOf(":") + 1)
+                    Some(simpleDateFormat.parse(chekingTime))
+
+                } catch {
+                    case e => None
+                }
+            }
+
+            checkinsWithMessage(TextLine(checkinsInput))
+
+                    .flatMapTo(
+                ('keyid, 'serType, 'serProfileID, 'serCheckinID, 'venName, 'venAddress, 'venId, 'chknTime, 'ghash, 'lat, 'lng, 'dayOfYear, 'dayOfWeek, 'hour, 'msg)
+                        ->
+                        ('serviceCheckinId, 'userProfileId, 'serType, 'serProfileID, 'serCheckinID, 'venName, 'venAddress, 'venId, 'chknTime, 'ghash, 'lat, 'lng, 'msg)
+            ) {
+
+                fields: (String, String, String, String, String, String, String, String, String, String, String, String, String, String, String) =>
+                    val (keyid, serType, serProfileID, serCheckinID, venName, venAddress, venId, chknTime, ghash, lat, lng, dayOfYear, dayOfWeek, hour, msg) = fields
+
+                    val gHashAsLong = Option(ghash).map(GeoHash.fromGeohashString(_).longValue()).getOrElse(0L)
+                    getDate(chknTime) map {
+                        checkinTime =>
+                            (serType + ":" + venId, keyid, serType, hashed(serProfileID), serCheckinID, venName, venAddress, venId, checkinTime, gHashAsLong, lat.toDouble, lng.toDouble, msg)
+                    }
+            }
+
+
+        case None =>
+            CassandraSource(
+                rpcHost = rpcHostArg.get,
+                privatePublicIpMap = ppmap,
+                keyspaceName = "dossier",
+                columnFamilyName = "Checkin",
+                scheme = NarrowRowScheme(keyField = 'serviceCheckinIdBuffer,
+                    nameFields = ('userProfileIdBuffer, 'serTypeBuffer, 'serProfileIDBuffer, 'serCheckinIDBuffer,
+                            'venNameBuffer, 'venAddressBuffer, 'venIdBuffer, 'chknTimeBuffer,
+                            'ghashBuffer, 'latBuffer, 'lngBuffer, 'msgBuffer),
+                    columnNames = List("userProfileId", "serviceType", "serviceProfileId",
+                        "serviceCheckinId", "venueName", "venueAddress",
+                        "venueId", "checkinTime", "geohash", "latitude",
+                        "longitude", "message"))
+            ).flatMap(('serviceCheckinIdBuffer, 'userProfileIdBuffer, 'serTypeBuffer, 'serProfileIDBuffer, 'serCheckinIDBuffer,
+                    'venNameBuffer, 'venAddressBuffer, 'venIdBuffer, 'chknTimeBuffer,
+                    'ghashBuffer, 'latBuffer, 'lngBuffer, 'msgBuffer) ->('serviceCheckinId, 'userProfileId, 'serType, 'serProfileID, 'serCheckinID,
+                    'venName, 'venAddress, 'venId, 'chknTime, 'ghash, 'lat, 'lng, 'msg)) {
+                in: (ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer,
+                        ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer) => {
+                    val rowKeyDes = StringSerializer.get().fromByteBuffer(in._1)
+                    val keyId = Option(in._2).map(StringSerializer.get().fromByteBuffer).getOrElse("missingKeyId")
+                    val serType = Option(in._3).map(StringSerializer.get().fromByteBuffer).orNull
+                    val serProfileID = Option(in._4).map(StringSerializer.get().fromByteBuffer).orNull
+                    val serCheckinID = Option(in._5).map(StringSerializer.get().fromByteBuffer).orNull
+                    val venName = Option(in._6).map(StringSerializer.get().fromByteBuffer).orNull
+                    val venAddress = Option(in._7).map(StringSerializer.get().fromByteBuffer).orNull
+                    val venId = Option(in._8).map(StringSerializer.get().fromByteBuffer).orNull
+                    val chknTime = Option(in._9).map(DateSerializer.get().fromByteBuffer).getOrElse(RichDate(0L))
+                    val ghash = Option(in._10).map(LongSerializer.get().fromByteBuffer).orNull
+                    val lat: Double = Option(in._11).map(DoubleSerializer.get().fromByteBuffer).orNull
+                    val lng: Double = Option(in._12).map(DoubleSerializer.get().fromByteBuffer).orNull
+                    val msg = Option(in._13).map(StringSerializer.get().fromByteBuffer).orNull
+                    // only checkins with venues
+                    if (CommonFunctions.isNullOrEmpty(venId))
+                        None
+                    else
+                        Some((rowKeyDes, keyId, serType, serProfileID, serCheckinID,
+                                venName, venAddress, venId, chknTime, ghash, lat, lng, msg))
+                }
+            }
+    }
 
     val deals = Tsv(dealsInput, ('dealId, 'successfulDeal, 'merchantName, 'majorCategory, 'minorCategory, 'minPricepoint, 'locationJSON)).map(('merchantName, 'locationJSON) ->('stemmedMerchantName, 'lat, 'lng, 'merchantGeosector)) {
         in: (String, String) =>
@@ -36,48 +123,6 @@ class DealAnalysis(args: Args) extends Job(args) with PlacesCorrelation with Che
             (stemmedMerchantName, dealLocation.latitude, dealLocation.longitude, geohash.longValue())
     }.discard('locationJSON)
 
-
-    val checkins = CassandraSource(
-        rpcHost = rpcHostArg,
-        privatePublicIpMap = ppmap,
-        keyspaceName = "dossier",
-        columnFamilyName = "Checkin",
-        scheme = NarrowRowScheme(keyField = 'serviceCheckinIdBuffer,
-            nameFields = ('userProfileIdBuffer, 'serTypeBuffer, 'serProfileIDBuffer, 'serCheckinIDBuffer,
-                    'venNameBuffer, 'venAddressBuffer, 'venIdBuffer, 'chknTimeBuffer,
-                    'ghashBuffer, 'latBuffer, 'lngBuffer, 'msgBuffer),
-            columnNames = List("userProfileId", "serviceType", "serviceProfileId",
-                "serviceCheckinId", "venueName", "venueAddress",
-                "venueId", "checkinTime", "geohash", "latitude",
-                "longitude", "message"))
-    ).flatMap(('serviceCheckinIdBuffer, 'userProfileIdBuffer, 'serTypeBuffer, 'serProfileIDBuffer, 'serCheckinIDBuffer,
-            'venNameBuffer, 'venAddressBuffer, 'venIdBuffer, 'chknTimeBuffer,
-            'ghashBuffer, 'latBuffer, 'lngBuffer, 'msgBuffer) ->('serviceCheckinId, 'userProfileId, 'serType, 'serProfileID, 'serCheckinID,
-            'venName, 'venAddress, 'venId, 'chknTime, 'ghash, 'lat, 'lng, 'msg)) {
-        in: (ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer,
-                ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer) => {
-            val rowKeyDes = StringSerializer.get().fromByteBuffer(in._1)
-            val keyId = Option(in._2).map(StringSerializer.get().fromByteBuffer).getOrElse("missingKeyId")
-            val serType = Option(in._3).map(StringSerializer.get().fromByteBuffer).orNull
-            val serProfileID = Option(in._4).map(StringSerializer.get().fromByteBuffer).orNull
-            val serCheckinID = Option(in._5).map(StringSerializer.get().fromByteBuffer).orNull
-            val venName = Option(in._6).map(StringSerializer.get().fromByteBuffer).orNull
-            val venAddress = Option(in._7).map(StringSerializer.get().fromByteBuffer).orNull
-            val venId = Option(in._8).map(StringSerializer.get().fromByteBuffer).orNull
-            val chknTime = Option(in._9).map(DateSerializer.get().fromByteBuffer).getOrElse(RichDate(0L))
-            val ghash = Option(in._10).map(LongSerializer.get().fromByteBuffer).orNull
-            val lat: Double = Option(in._11).map(DoubleSerializer.get().fromByteBuffer).orNull
-            val lng: Double = Option(in._12).map(DoubleSerializer.get().fromByteBuffer).orNull
-            val msg = Option(in._13).map(StringSerializer.get().fromByteBuffer).orNull
-            // only checkins with venues
-            if (CommonFunctions.isNullOrEmpty(venId))
-                None
-            else
-                Some((rowKeyDes, keyId, serType, serProfileID, serCheckinID,
-                        venName, venAddress, venId, chknTime, ghash, lat, lng, msg))
-        }
-    }
-
     val levenshtein = new Levenshtein()
 
     val newCheckins = correlationCheckinsFromCassandra(checkins)
@@ -85,9 +130,9 @@ class DealAnalysis(args: Args) extends Job(args) with PlacesCorrelation with Che
             .joinWithTiny('geosector -> 'merchantGeosector, deals).groupBy('geosector) {
         _.sortWithTake(('stemmedVenName -> 'stemmedMerchantName) -> 'singleVenue, 1) {
             (a: (String, String), b: (String, String)) => levenshtein.compareInt(a._1, a._2) < levenshtein.compareInt(b._1, b._2)
-        }.head('*)
+        }.head('goldenId, 'merchantName)
     }
-    dealVenues.write(SequenceFile(dealsOutput, '*))
+    dealVenues.write(SequenceFile(dealsOutput, ('goldenId, 'merchantName)))
 }
 
 object DealAnalysis {
