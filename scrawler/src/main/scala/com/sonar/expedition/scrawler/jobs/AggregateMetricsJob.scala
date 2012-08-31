b9961189c565ae51f3d90b1e5f9e4e874a2bda8e
Ben Kempe <ben@k2medialabs.com>
2012-08-31 18:21:03 -0400

    aggregate

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/AggregateMetricsJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/AggregateMetricsJob.scala
@@ -5,14 +5,18 @@ import cascading.tuple.Fields
 
 class AggregateMetricsJob(args: Args) extends Job(args) {
     val sequenceOutputStatic = args("sequenceOutputStatic")
+    val dealsOutput = args("dealsOutput")
     val metricsOut = args("metricsOut")
-    SequenceFile(sequenceOutputStatic, ('rowKey, 'columnName, 'columnValue)).read
+    val metrics = SequenceFile(sequenceOutputStatic, ('rowKey, 'columnName, 'columnValue)).read
             .mapTo(('rowKey, 'columnName, 'columnValue) ->('venueId, 'metric, 'value)) {
         in: (String, String, Double) =>
             val (rowKey, columnName, columnValue) = in
             val Array(venueId, metricPrefix) = rowKey.split("_", 2)
             (venueId, metricPrefix + "_" + columnName, columnValue)
-    }.groupBy('venueId) {
+    }
+
+    SequenceFile(dealsOutput, DealAnalysis.DealsOutputTuple).joinWithLarger('goldenId -> 'venueId, metrics)
+            .groupBy(DealAnalysis.DealsOutputTuple) {
         _.toList[(String, String)](('metric, 'value) -> 'metricValues)
     }.write(Tsv(metricsOut, Fields.ALL))
 
