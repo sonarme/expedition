f7d85f4fba8c3b15ca6cecddb7c69c14c15a7e16
Ben Kempe <ben@k2medialabs.com>
2012-09-04 17:15:30 -0400

    cleanup

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/util/CommonFunctions.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/util/CommonFunctions.scala
@@ -10,7 +10,6 @@ object CommonFunctions {
     val Occupation: Regex = """([a-zA-Z\d\- ]+)\t([a-zA-Z\d\- ,]+)\t([a-zA-Z\d\- ]+)\t(.+)""".r
     val FriendExtractLine: Regex = """([a-zA-Z\d\-]+):(.*?)"id":"(.*?)","service_type":"(.*?)","name":"(.*?)","photo(.*)""".r
     val FriendProdExtractLine: Regex = """([a-zA-Z\d\-]+)::(twitter|facebook|foursquare|linkedin|sonar)::([a-zA-Z\d\-]+)::(.*)""".r
-    val ServiceProfileExtractLineCheck: Regex = """^([a-zA-Z\d\-]+)_(fb|ln|tw|4s)\s:\s(.*)$""".r
     val pay: Regex = """(.*)([\d\,]+)(\s+)(per year)(.*)""".r
     val BuzzFromText: Regex = """(.*)\t([\d\.]+)\t([\d\.]+)\t(.*)""".r
 
