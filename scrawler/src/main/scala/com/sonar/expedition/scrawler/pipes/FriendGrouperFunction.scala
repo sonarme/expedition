47ed6c850c88f51a7e143f5d17624d99111200c1
Ben Kempe <ben@k2medialabs.com>
2012-09-05 18:05:10 -0400

    fix

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/pipes/FriendGrouperFunction.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/pipes/FriendGrouperFunction.scala
@@ -1,8 +1,10 @@
 package com.sonar.expedition.scrawler.pipes
 
+import cascading.tuple.Fields
+
 
 trait FriendGrouperFunction extends ScaldingImplicits {
-    val FriendTuple = ('userProfileId, 'serviceType, 'serviceProfileId)
+    val FriendTuple: Fields = ('userProfileId, 'serviceType, 'serviceProfileId)
 
 }
 
