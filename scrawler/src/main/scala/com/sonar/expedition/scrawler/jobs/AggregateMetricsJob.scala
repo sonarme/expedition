9bd758f4858e0c151b39938a40ec8133aaa454e4
Ben Kempe <ben@k2medialabs.com>
2012-09-04 20:43:04 -0400

    address and phone

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/AggregateMetricsJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/AggregateMetricsJob.scala
@@ -1,14 +1,16 @@
 package com.sonar.expedition.scrawler.jobs
 
-import com.twitter.scalding.{Tsv, SequenceFile, Args, Job}
+import com.twitter.scalding._
 import cascading.tuple.Fields
+import com.twitter.scalding.SequenceFile
+import com.twitter.scalding.Tsv
 
 class AggregateMetricsJob(args: Args) extends Job(args) {
-    val staticAnalysisOutput = args("staticAnalysisOutput")
+    val staticInputFiles = args("staticInputFiles").split(',')
     val dealsOutput = args("dealsOutput")
     val metricsOut = args("metricsOut")
-    val metrics = SequenceFile(staticAnalysisOutput, ('rowKey, 'columnName, 'columnValue)).read
-            .mapTo(('rowKey, 'columnName, 'columnValue) ->('venueId, 'metric, 'value)) {
+    val allPipes = staticInputFiles.map(file => SequenceFile(file, ('rowKey, 'columnName, 'columnValue)).read).reduce(_ ++ _)
+    val metrics = allPipes.mapTo(('rowKey, 'columnName, 'columnValue) ->('venueId, 'metric, 'value)) {
         in: (String, String, Double) =>
             val (rowKey, columnName, columnValue) = in
             val Array(venueId, metricPrefix) = rowKey.split("_", 2)
