1a7477f50c0e1c5cfd33b080dde9d5d7583b06cb
Roger Chang <roger@sonar.me>
2012-09-08 20:36:09 -0400

    parsejob

---
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/crawler/ParseJob.scala
@@ -0,0 +1,54 @@
+package com.sonar.expedition.scrawler.crawler
+
+import com.twitter.scalding.{Tsv, SequenceFile, Job, Args}
+import cascading.tuple.Fields
+
+class ParseJob(args: Args) extends Job(args) {
+
+    val level: Int = args("level").toInt
+    val levelUp: Int = level + 1
+    val outputDir = args("output")
+    val domains = args("domains")
+
+    val parsed = Tsv(outputDir + "/crawl_" + level + "/parsed.tsv", ('url, 'timestamp, 'businessName, 'category, 'rating, 'latitude, 'longitude, 'address, 'city, 'state, 'zip, 'phone, 'priceRange, 'reviewCount, 'likes, 'dealPrice, 'purchased, 'savingsPercent, 'dealDescription, 'dealImage))
+    val parsedSequence = SequenceFile(outputDir + "/crawl_" + level + "/parsed", ('url, 'timestamp, 'businessName, 'category, 'rating, 'latitude, 'longitude, 'address, 'city, 'state, 'zip, 'phone, 'priceRange, 'reviewCount, 'likes, 'dealPrice, 'purchased, 'savingsPercent, 'dealDescription, 'dealImage))
+
+    val parsed2 = Tsv(outputDir + "/crawl_" + level + "/parsed2.tsv")
+    val parsedSequence2 = SequenceFile(outputDir + "/crawl_" + level + "/parsed2", Fields.ALL)
+
+
+    val parsedAgain = parsed
+            .map(('url, 'dealPrice, 'purchased, 'savingsPercent) ->('dealPriceNum, 'purchasedNum, 'savingsPercentNum)) {
+        in: (String, String, String, String) =>
+            val (url, price, purchased, savings) = in
+            val priceNum = try {
+                val p = price.substring(1).replace(",", "").toInt
+                println(p)
+                p
+            } catch {
+                case e: Exception => println("url: " + url + "\n" + e); 0
+            }
+            val purchaseNum = try {
+                purchased.replace(",", "").toInt
+            } catch {
+                case e: Exception => println("url: " + url + "\n" + e); try {
+                    purchased.replace(",", "").replace("?", "").toInt
+                } catch {
+                    case f: Exception => 0
+                }
+            }
+            val savingsNum = try {
+                savings.stripSuffix("%").toInt
+            } catch {
+                case e: Exception => println("url: " + url + "\n" + e); 0
+            }
+            (priceNum, purchaseNum, savingsNum)
+    }
+            .discard('dealPrice, 'purchased, 'savingsPercent)
+
+    parsedAgain
+        .write(parsed2)
+
+    parsedAgain
+        .write(parsedSequence2)
+}
