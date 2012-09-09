139e525693126113b73ef47ce6b64047a3411dd1
Ben Kempe <ben@k2medialabs.com>
2012-09-09 13:04:04 -0400

    feature ext

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/TestJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/TestJob.scala
@@ -1,11 +1,20 @@
 package com.sonar.expedition.scrawler.jobs
 
-import com.twitter.scalding.{Tsv, TextLine, Job, Args}
+import com.twitter.scalding._
 import cascading.tuple.{Tuple, Fields}
+import com.twitter.scalding.Tsv
+import scala.Some
 
 class TestJob(args: Args) extends Job(args) with CheckinSource {
-    val (checkins, _) = checkinSource(args, false, false)
-    checkins.groupBy('venId) {
+    val checkins = args.optional("input") match {
+        case Some(input) => Tsv(input, ('venId, 'keyid, 'c)).read: RichPipe
+        case _ => checkinSource(args, false, false)._1
+    }
+    checkins.map('venId -> 'bla) {
+        x: String =>
+            val set = ((1 to 10000) map (x + _)).toSet[String]
+            set
+    }.groupBy('venId) {
         _.foldLeft('keyid -> 'q)(Map.empty[String, Int]) {
             (agg: Map[String, Int], f: String) => agg + (f -> (agg.getOrElse(f, 0) + 1))
         }
