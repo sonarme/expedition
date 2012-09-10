baf50e4da3eae985813adaadad79022648b63012
Paul Fisher <paul@k2media.net>
2012-09-10 01:44:42 -0400

    fix rounding issue and removed extra fields

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/JoinDealJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/JoinDealJob.scala
@@ -9,7 +9,7 @@ class JoinDealJob(args: Args) extends Job(args) {
         new Fields("enabled", "dealId", "successfulDeal", "goldenId", "venName", "merchantName", "majorCategory", "minorCategory", "minPricepoint", "loyalty_customerCount_Passers-By", "loyalty_customerCount_Regulars", "loyalty_customerCount_Addicts", "loyalty_visitCount_Passers-By", "loyalty_visitCount_Regulars", "loyalty_visitCount_Addicts", "numCheckins_all", "numCheckins_withProfile", "reach_distance_meanDist", "reach_distance_stdevDist", "reach_originCount_numHome", "reach_originCount_numWork", "ageAve", "ageStdev", "age_<18", "age_18-24", "age_25-35", "age_35-44", "age_45-54", "age_55-64", "age_65+", "gender_male", "gender_female", "education_College", "education_No College", "education_Grad School", "education_unknown", "income_$0-50k", "income_$50-100k", "income_$100-150k", "income_$150k+", "yrating", "ypriceRangeMetric", "yreviewCount", "yelpDealId")).read
     val ls = SequenceFile(args("livingsocial"), ('url, 'timestamp, 'merchantName, 'majorCategory, 'rating, 'merchantLat, 'merchantLng, 'merchantAddress, 'city, 'state, 'zip, 'merchantPhone, 'priceRange, 'reviewCount, 'likes, 'dealDescription, 'dealImage, 'dealRegion, 'dealPrice, 'purchased, 'savingsPercent)).read.map('url -> 'dealId1) {
         url: String => url.split('/').last.split('-').head
-    }.project('dealId1, 'reviewCount, 'likes, 'dealPrice, 'purchased, 'city, 'state, 'zip, 'merchantPhone, 'priceRange, 'dealDescription, 'dealImage, 'dealRegion, 'savingsPercent).map('dealDescription -> 'editedDealDescription) {
+    }.project('dealId1, 'reviewCount, 'likes, 'dealPrice, 'purchased/*, 'city, 'state, 'zip*/, 'merchantPhone, 'priceRange/*, 'dealDescription*/, 'dealImage, 'dealRegion, 'savingsPercent).map('dealDescription -> 'editedDealDescription) {
         (descriptionField: String) => descriptionField match {
             case (description: String) if description != null => description.replaceAll( """\s""", "_")
             case _ => ""
@@ -18,23 +18,27 @@ class JoinDealJob(args: Args) extends Job(args) {
         in: (Double, Int) => {
         val (dealPrice, purchased) = in
             val revenue = dealPrice * purchased
-            val dealSuccess = revenue > 1000
+            val dealSuccess = revenue > 1800
             (revenue, dealSuccess)
         }
     }
     val reveneAnalysisPipe = ls.groupAll(_.sizeAveStdev('revenue -> ('revenueCount, 'revenueAverage, 'revenueStdDev))
-            .max('revenue -> 'maxRevenue)
-            .min('revenue -> 'minRevenue))
+            /*.max('revenue -> 'maxRevenue)
+            .min('revenue -> 'minRevenue)*/)
 
     val lsWithStats = ls.crossWithTiny(reveneAnalysisPipe).map(('revenue, 'revenueAverage, 'revenueStdDev) -> 'revenueBucket) {
         in: (Double, Double, Double) =>
             val (revenue, revenueAverage, revenueStdDev) = in
-            val offset = revenueAverage / revenueStdDev
+            val offset = math.round(revenueAverage / revenueStdDev)
             offset + math.round((revenue - revenueAverage) / revenueStdDev)
     }
 
-//    val lsSample = lsWithStats.limit(1000).groupAll(_.sortBy('revenue).mapStream())
+   /* val lsSample = lsWithStats.limit(1000).groupAll(_.sortedTake[Int]('revenue -> 'sortedRevenue, 1000).mapStream('sortedRevenue, 'revenueBuckets) {
+        in: (data)
+    })*/
 
-    input.leftJoinWithLarger('dealId -> 'dealId1, lsWithStats).write(Tsv(args("output"), Fields.ALL))
+
+    List().scanLeft()
+    input.leftJoinWithLarger('dealId -> 'dealId1, lsWithStats).write(Tsv(args("output"), Fields.ALL, true, true))
 }
 
