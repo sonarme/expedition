391d4f35ec45c1643ae4e92251d17d9589173804
Ben Kempe <ben@k2medialabs.com>
2012-09-11 00:33:42 -0400

    yelp combiner

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/YelpSequenceFileCombineJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/YelpSequenceFileCombineJob.scala
@@ -1,6 +1,6 @@
 package com.sonar.expedition.scrawler.jobs
 
-import com.twitter.scalding.{SequenceFile, Args, Job}
+import com.twitter.scalding.{Tsv, SequenceFile, Args, Job}
 import cascading.tuple.Fields
 
 class YelpSequenceFileCombineJob(args: Args) extends Job(args) {
@@ -11,5 +11,6 @@ class YelpSequenceFileCombineJob(args: Args) extends Job(args) {
         _.head(new Fields("successfulDeal", "goldenId", "venName", "venueLat", "venueLng", "merchantName", "merchantAddress", "merchantCity", "merchantState", "merchantZip", "merchantPhone", "majorCategory", "minorCategory", "minPricepoint", "rating", "priceRange", "reviewCount", "likes", "purchased", "savingsPercent", "venueSector", "yurl", "ybusinessName", "ycategory", "yrating", "ylatitude", "ylongitude", "yaddress", "ycity", "ystate", "yzip", "yphone", "ypriceRange", "yreviewCount", "yreviews")
                 -> new Fields("successfulDeal", "goldenId", "venName", "venueLat", "venueLng", "merchantName", "merchantAddress", "merchantCity", "merchantState", "merchantZip", "merchantPhone", "majorCategory", "minorCategory", "minPricepoint", "rating", "priceRange", "reviewCount", "likes", "purchased", "savingsPercent", "venueSector", "yurl", "ybusinessName", "ycategory", "yrating", "ylatitude", "ylongitude", "yaddress", "ycity", "ystate", "yzip", "yphone", "ypriceRange", "yreviewCount", "yreviews"))
     }.write(SequenceFile(args("output"), NewAggregateMetricsJob.Reviews))
+            .write(Tsv(args("output") + "_tsv", NewAggregateMetricsJob.Reviews))
 }
 
