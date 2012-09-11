bc9d12cbd0ba74a16e2e3e14b82eb6f0e580ca9a
Ben Kempe <ben@k2medialabs.com>
2012-09-11 00:25:46 -0400

    yelp

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/NewAggregateMetricsJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/NewAggregateMetricsJob.scala
@@ -26,6 +26,11 @@ class NewAggregateMetricsJob(args: Args) extends Job(args) {
     } /*.joinWithSmaller('goldenId -> 'goldenId1, numCheckins.rename('goldenId -> 'goldenId1)).discard('goldenId1)*/
 
     val yelp = SequenceFile(args("yelp"), NewAggregateMetricsJob.Reviews).read.project('dealId, 'yrating, 'yreviewCount)
+    /*.map(('yrating, 'yreviewCount)->'q) {
+        in:(String,String) =>
+                in
+            ""
+    }*/
     val deals = SequenceFile(dealsOutput, DealAnalysis.DealsOutputTuple).read
             .leftJoinWithSmaller('dealId -> '_dealId, yelp.rename('dealId -> '_dealId)).discard('_dealId)
     val fields = NewAggregateMetricsJob.NonFeatureTuple.append(('yrating, 'yreviewCount))
@@ -38,7 +43,7 @@ class NewAggregateMetricsJob(args: Args) extends Job(args) {
                     name -> (if (name.startsWith("num") || NewAggregateMetricsJob.IntValues(name)) try {
                         value.toString.toInt
                     } catch {
-                        case _: NumberFormatException => 0
+                        case _: NumberFormatException => -1
                     } else value)
             }
             import collection.JavaConversions._
@@ -62,45 +67,6 @@ object NewAggregateMetricsJob extends FieldConversions {
     val DealMetrics = NonFeatureTuple.iterator().toIterable.map(_.toString)
     val Reviews =
         new Fields("dealId", "successfulDeal", "goldenId", "venName", "venueLat", "venueLng", "merchantName", "merchantAddress", "merchantCity", "merchantState", "merchantZip", "merchantPhone", "majorCategory", "minorCategory", "minPricepoint", "rating", "priceRange", "reviewCount", "likes", "purchased", "savingsPercent", "venueSector", "yurl", "ybusinessName", "ycategory", "yrating", "ylatitude", "ylongitude", "yaddress", "ycity", "ystate", "yzip", "yphone", "ypriceRange", "yreviewCount", "yreviews")
-    /*
 
-            new Fields("dealId", "successfulDeal", "merchantName", "majorCategory", "minorCategory", "minPricepoint", "address", "city", "state", "zip", "lat", "lng", "link", "ybusinessName", "ycategory", "rating", "ylatitude", "ylongitude", "yaddress", "ycity", "ystate", "yzip", "yphone", "ypriceRange", "reviewCount", "yreviews")
-    */
 
-    def reviewTuple(name: String) = new Fields(name + "_rating", name + "_reviewCount")
-
-    val MetricsFields = new Fields(
-        "loyalty_customerCount_Passers-By",
-        "loyalty_customerCount_Regulars",
-        "loyalty_customerCount_Addicts",
-        "loyalty_visitCount_Passers-By",
-        "loyalty_visitCount_Regulars",
-        "loyalty_visitCount_Addicts",
-        "numCheckins_all",
-        "numCheckins_withProfile",
-        "reach_distance_meanDist",
-        "reach_distance_stdevDist",
-        "reach_originCount_numHome",
-        "reach_originCount_numWork",
-        "ageAve",
-        "ageStdev",
-        "age_<18",
-        "age_18-24",
-        "age_25-35",
-        "age_35-44",
-        "age_45-54",
-        "age_55-64",
-        "age_65+",
-        "gender_male",
-        "gender_female",
-        "education_College",
-        "education_No College",
-        "education_Grad School",
-        "education_unknown",
-        "income_$0-50k",
-        "income_$50-100k",
-        "income_$100-150k",
-        "income_$150k+"
-    )
-    val OutputFormat = (('dealId, 'successfulDeal, 'goldenId, 'venName, 'merchantName /*'venAddress, 'venuePhone,  'merchantAddress, 'merchantPhone, 'distance, 'levenshtein*/ ): Fields).append(MetricsFields).append(('yrating, 'ypriceRangeMetric, 'yreviewCount))
 }
