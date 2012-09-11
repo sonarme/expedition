3095416c73867c35b4ad402ced3a8c37217f4f01
Ben Kempe <ben@k2medialabs.com>
2012-09-11 01:05:04 -0400

    yelp

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/YelpSequenceFileCombineJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/YelpSequenceFileCombineJob.scala
@@ -4,10 +4,10 @@ import com.twitter.scalding.{Tsv, SequenceFile, Args, Job}
 import cascading.tuple.Fields
 
 class YelpSequenceFileCombineJob(args: Args) extends Job(args) {
-    val file1 = SequenceFile(args("input1"), NewAggregateMetricsJob.Reviews).read
-    val file2 = SequenceFile(args("input2"), NewAggregateMetricsJob.Reviews).read
-    val file3 = SequenceFile(args("input3"), NewAggregateMetricsJob.Reviews).read
-    (file1 ++ file2 ++ file3).groupBy('dealId) {
+    val merged = args("inputs").split(',').map {
+        file => SequenceFile(file, NewAggregateMetricsJob.Reviews).read
+    }.reduce(_ ++ _)
+    merged.groupBy('dealId) {
         _.head(new Fields("successfulDeal", "goldenId", "venName", "venueLat", "venueLng", "merchantName", "merchantAddress", "merchantCity", "merchantState", "merchantZip", "merchantPhone", "majorCategory", "minorCategory", "minPricepoint", "rating", "priceRange", "reviewCount", "likes", "purchased", "savingsPercent", "venueSector", "yurl", "ybusinessName", "ycategory", "yrating", "ylatitude", "ylongitude", "yaddress", "ycity", "ystate", "yzip", "yphone", "ypriceRange", "yreviewCount", "yreviews")
                 -> new Fields("successfulDeal", "goldenId", "venName", "venueLat", "venueLng", "merchantName", "merchantAddress", "merchantCity", "merchantState", "merchantZip", "merchantPhone", "majorCategory", "minorCategory", "minPricepoint", "rating", "priceRange", "reviewCount", "likes", "purchased", "savingsPercent", "venueSector", "yurl", "ybusinessName", "ycategory", "yrating", "ylatitude", "ylongitude", "yaddress", "ycity", "ystate", "yzip", "yphone", "ypriceRange", "yreviewCount", "yreviews"))
     }.write(SequenceFile(args("output"), NewAggregateMetricsJob.Reviews))
