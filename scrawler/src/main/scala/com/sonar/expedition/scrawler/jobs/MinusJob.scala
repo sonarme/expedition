ffd4b9d5df3ebc03cb1c90222a2854a1a23f5635
Ben Kempe <ben@k2medialabs.com>
2012-09-27 11:24:25 -0400

    cleanup

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/MinusJob.scala
+++
@@ -1,9 +0,0 @@
-package com.sonar.expedition.scrawler.jobs
-
-import com.twitter.scalding.{Tsv, Args, Job}
-
-class MinusJob(args: Args) extends Job(args) {
-    Tsv(args("a"), 'a).leftJoinWithLarger('a -> 'b, Tsv(args("b"), 'b)).filter(('a, 'b)) {
-        in: (String, String) => in._2 == null
-    }.write(Tsv(args("c"), 'a))
-}
