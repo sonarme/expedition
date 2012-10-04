8aa77881b72b44d9357a2562cd64a0985d2aab5d
Ben Kempe <ben@k2medialabs.com>
2012-10-04 11:11:38 -0400

    individual checkin

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/IndividualCheckinJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/IndividualCheckinJob.scala
@@ -1,10 +1,12 @@
 package com.sonar.expedition.scrawler.jobs
 
 import com.twitter.scalding.{SequenceFile, Args, Job}
+import com.sonar.expedition.scrawler.util
 
 class IndividualCheckinJob(args: Args) extends Job(args) with CheckinSource {
     val (checkins, _) = checkinSource(args, false, false)
-    val individual = args("keyid")
+    val Array(serviceType, serviceId) = args("serviceProfileId").split(':')
+    val individual = serviceType + ":" + util.CommonFunctions.hashed(serviceId)
     checkins.filter('keyid) {
         keyid: String => keyid == individual
     }.write(SequenceFile(args("output")))
