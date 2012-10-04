86882f2dce741e96bc186f305ff37c4de6114710
Ben Kempe <ben@k2medialabs.com>
2012-10-04 14:46:01 -0400

    cleanup

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/pipes/CheckinInfoPipe.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/pipes/CheckinInfoPipe.scala
@@ -13,30 +13,6 @@ import com.sonar.expedition.scrawler.util.LocationClusterer
 
 trait CheckinInfoPipe extends ScaldingImplicits {
 
-    def findCityofUserFromChkins(chkins: RichPipe): RichPipe = {
-        val citypipe = chkins.groupBy(('key, 'uname, 'fbid, 'lnid, 'mtphnWorked, 'city, 'worktitle)) {
-            _
-                    //.mapReduceMap('key->'key1), todo understand mapreducemap api
-                    .toList[String]('loc -> 'locList)
-
-        }.mapTo(('key, 'uname, 'fbid, 'lnid, 'mtphnWorked, 'city, 'worktitle, 'locList) ->('workco, 'name, 'wrkcity, 'wrktitle, 'fb, 'ln)) {
-            fields: (String, String, String, String, String, String, String, List[String]) =>
-                val (key, uname, fbid, lnid, worked, city, worktitle, chkinlist) = fields
-                if (city == "null") {
-                    val chkcity = findCityFromChkins(chkinlist)
-                    (worked, uname, chkcity, worktitle, fbid, lnid)
-                } else {
-                    (worked, uname, city, worktitle, fbid, lnid)
-                }
-        }.mapTo(('workco, 'name, 'wrkcity, 'wrktitle, 'fb, 'ln) ->('mtphnWorked, 'uname, 'city, 'worktitle, 'fbid, 'lnid)) {
-            fields: (String, String, String, String, String, String) =>
-                val (worked, uname, city, worktitle, fbid, lnid) = fields
-                (worked, uname, city, worktitle, fbid, lnid)
-        }.project(('mtphnWorked, 'uname, 'city, 'worktitle, 'fbid, 'lnid))
-
-        citypipe
-    }
-
     // todo: mr-dbscan
     def findClusteroidofUserFromChkins(chkins: RichPipe) =
         chkins.groupBy('key) {
