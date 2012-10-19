e764f6346b7cb26871aafa923a017a8c842efca0
Roger Chang <roger@sonar.me>
2012-10-19 15:24:30 -0400

    checkin to geoname

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/CheckinToGeonameRDF.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/CheckinToGeonameRDF.scala
@@ -5,6 +5,10 @@ import com.sonar.expedition.scrawler.util.Tuples
 import collection.JavaConversions._
 import com.sonar.expedition.scrawler.util.CommonFunctions._
 import com.hp.hpl.jena.rdf.model.ModelFactory
+import com.hp.hpl.jena.vocabulary.RDF
+import com.sonar.expedition.scrawler.util.RDFNamespaces._
+import java.io.StringWriter
+import com.hp.hpl.jena.shared.CannotEncodeCharacterException
 
 class CheckinToGeonameRDF(args: Args) extends Job(args) {
     val input = args("input")
@@ -13,20 +17,45 @@ class CheckinToGeonameRDF(args: Args) extends Job(args) {
     val checkins = SequenceFile(input, Tuples.Place)
     val geonameRDF = Tsv(outputDir + "/geonameRdf_tsv")
 
-    val checkinsSmall = Tsv(outputDir + "checkinsSmall_tsv")
+    val checkinsSmall = Tsv(outputDir + "/checkinsSmall_tsv")
+
+    val models = checkins
+            .read
+            .flatMapTo(('serType, 'venId, 'venName, 'venAddress, 'lat, 'lng) -> 'model) {
+        x: (String, String, String, String, Double, Double) =>
+            val (serType, venId, venName, venAddress, lat, lng) = x
+            val model = ModelFactory.createDefaultModel()
+            model.setNsPrefixes(Map[String, String](
+                "gn" -> Gn,
+                "wgs84_pos" -> Wgs84_pos,
+                "foaf" -> Foaf,
+                "owl" -> Owl,
+                "rdf" -> Rdf,
+                "rdfs" -> Rdfs,
+                "sonar" -> Sonar)
+            )
+
+            model.createResource(Gn + venId)
+                    .addProperty(RDF.`type`, model.createResource(Gn + "Feature"))
+                    .addProperty(model.createProperty(Gn, "name"), venName)
+                    .addProperty(model.createProperty(Wgs84_pos, "lat"), lat.toString)
+                    .addProperty(model.createProperty(Wgs84_pos, "lng"), lng.toString)
+                    .addProperty(model.createProperty(Sonar, "address"), venAddress)
 
 
-    val gn = "http://www.geonames.org/ontology#"
-    val wgs84_pos = "http://www.w3.org/2003/01/geo/wgs84_pos#"
+            val strWriter = new StringWriter
+            try {
+                model.write(strWriter, "RDF/XML-ABBREV")
+                val str = strWriter.toString
+                //we strip the root element so that we can create one big document.  should put it back in somewhere
+                val strippedString = strWriter.toString.substring(str.indexOf("  <gn:Feature "), str.lastIndexOf("</rdf:RDF>"))
+                Some("  " + strippedString.trim)
+            } catch {
+                case cece: CannotEncodeCharacterException => throw new RuntimeException("Failed creating model for " + venId, cece)
+            } finally {
+                strWriter.close()
+            }
+    }
 
-    val model = ModelFactory.createDefaultModel()
-    model.setNsPrefixes(Map[String, String](
-                            "gn" -> gn,
-                            "wgs84_pos" -> wgs84_pos)
-                        )
-
-    model.createResource()
-    checkins
-        .read
-        .write(checkinsSmall)
+    models.write(geonameRDF)
 }
---
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/util/RDFNamespaces.scala
@@ -0,0 +1,11 @@
+package com.sonar.expedition.scrawler.util
+
+object RDFNamespaces {
+    val Gn = "http://www.geonames.org/ontology#"
+    val Wgs84_pos = "http://www.w3.org/2003/01/geo/wgs84_pos#"
+    val Foaf = "http://xmlns.com/foaf/0.1/"
+    val Owl = "http://www.w3.org/2002/07/owl#"
+    val Rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    val Rdfs = "http://www.w3.org/2000/01/rdf-schema#"
+    val Sonar = "http://sonar.me/#"
+}
