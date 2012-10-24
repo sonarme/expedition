12b608091da3688c5dbfbc5e92381359d2af10d7
Ben Kempe <ben@k2medialabs.com>
2012-10-24 18:46:50 -0400

    sparql update

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/ServiceProfileToRDFJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/ServiceProfileToRDFJob.scala
@@ -1,26 +1,21 @@
 package com.sonar.expedition.scrawler.jobs
 
-import com.twitter.scalding.{SequenceFile, Tsv, Job, Args}
-import com.sonar.scalding.cassandra.{WideRowScheme, CassandraSource}
-import java.nio.ByteBuffer
-import me.prettyprint.cassandra.serializers.StringSerializer
+import com.twitter.scalding._
 import com.sonar.expedition.scrawler.pipes.{FriendGrouperFunction, DTOProfileInfoPipe}
-import com.hp.hpl.jena.rdf.model.{ResourceFactory, Model, ModelFactory}
-import collection.JavaConversions._
-import com.sonar.expedition.scrawler.util.CommonFunctions._
-import java.io.{StringWriter, ByteArrayOutputStream}
-import com.hp.hpl.jena.ontology.OntModel
 import com.sonar.expedition.scrawler.jena.vocabulary.OPO
-import com.sonar.expedition.scrawler.json.ScrawlerObjectMapper
-import com.sonar.dossier.dto.ServiceProfileDTO
 import com.hp.hpl.jena.sparql.vocabulary.FOAF
-import thewebsemantic.vocabulary.Sioc
 import com.sonar.dossier.ScalaGoodies._
-import com.hp.hpl.jena.shared.CannotEncodeCharacterException
-
-//import org.apache.clerezza.rdf.ontologies.FOAF
-
+import com.sonar.expedition.scrawler.util.Tuples
+import com.hp.hpl.jena.update.UpdateExecutionFactory
+import com.hp.hpl.jena.sparql.modify.request.UpdateDeleteInsert
+import java.text.SimpleDateFormat
+import com.sonar.dossier.dto.ServiceProfileDTO
+import com.twitter.scalding.SequenceFile
+import com.twitter.scalding.TextLine
+import com.hp.hpl.jena.rdf.model.{StmtIterator, ResourceFactory, ModelFactory}
 import com.hp.hpl.jena.vocabulary._
+import com.hp.hpl.jena.sparql.core.DatasetImpl
+
 
 // Use args:
 // STAG while local testing: --rpcHost 184.73.11.214 --ppmap 10.4.103.222:184.73.11.214,10.96.143.88:50.16.106.193
@@ -30,7 +25,7 @@ class ServiceProfileToRDFJob(args: Args) extends Job(args) with DTOProfileInfoPi
     val input = args("input")
 
     //    val profilesTsv = Tsv("/Users/rogchang/Desktop/rdf/serviceProfiles.tsv", ProfileTuple) //('key, 'uname, 'fbid, 'lnid, 'fsid, 'twid, 'educ, 'worked, 'city, 'edegree, 'eyear, 'worktitle, 'workdesc)
-    val profiles = SequenceFile(input, ('userProfileId, 'serviceType, 'json))
+    val profiles = SequenceFile(input, Tuples.Profile)
     //    val friendships = SequenceFile("/Users/rogchang/Desktop/rdf/friendship_prod_0905_small", FriendTuple)
 
 
@@ -41,8 +36,7 @@ class ServiceProfileToRDFJob(args: Args) extends Job(args) with DTOProfileInfoPi
         val testOutput2 = Tsv(outputDir + "/test2.tsv")
     */
 
-    val profileRdf = Tsv(outputDir + "/profileRdfTsv")
-    val profileRdfSequence = SequenceFile(outputDir + "/profileRdfSequence")
+    val profileRdf = TextLine(outputDir + "/profileRdf")
     //  val profilesSmall = Tsv(outputDir + "/profilesSmall.tsv")
 
     //   val profilesSmall2 = Tsv(input, ('userProfileId, 'serviceType, 'json))
@@ -55,55 +49,68 @@ class ServiceProfileToRDFJob(args: Args) extends Job(args) with DTOProfileInfoPi
 
     val models = profiles
             .read
-            .filter('json) {
-        json: String => json.startsWith("{")
-    }
-            .groupBy('userProfileId, 'serviceType) {
-        _.head('json)
-    }
-            .flatMapTo('json -> 'model) {
-        json: String => {
-            ScrawlerObjectMapper.parseJson[ServiceProfileDTO](json) flatMap {
-                serviceProfile =>
-                    val model = ModelFactory.createDefaultModel()
-                    //todo: find SIOC library
-                    model.setNsPrefixes(Map[String, String](
-                        "foaf" -> foaf,
-                        "sioc" -> sioc,
-                        "opo" -> opo,
-                        "sonar" -> sonar)
-                    )
+            .mapTo('profile -> 'model) {
+        serviceProfile: ServiceProfileDTO => {
+            val model = ModelFactory.createDefaultModel()
+            //todo: find SIOC library
 
-                    model.createResource()
-                            .addProperty(RDF.`type`, FOAF.Person)
-                            .addProperty(ResourceFactory.createProperty(foaf + "account"), model.createResource(sioc + serviceProfile.serviceType + ":" + serviceProfile.userId)
-                            .addProperty(RDF.`type`, ResourceFactory.createProperty(sioc + "UserAccount"))
-                            .addProperty(FOAF.name, ??(serviceProfile.fullName).getOrElse(""))
-                            .addProperty(FOAF.gender, ??(serviceProfile.gender).getOrElse("").toString)
-                            .addProperty(FOAF.birthday, ??(serviceProfile.birthday).getOrElse("").toString)
-                            .addProperty(FOAF.accountName, ??(serviceProfile.userId).getOrElse(""))
-                            .addProperty(FOAF.mbox, ??(serviceProfile.aliases.email).getOrElse(""))
-                            .addProperty(model.createProperty(foaf + "twitterId"), ??(serviceProfile.aliases.twitter).getOrElse(""))
-                            .addProperty(model.createProperty(foaf + "facebookId"), ??(serviceProfile.aliases.facebook).getOrElse(""))
-                            .addProperty(FOAF.homepage, ??(serviceProfile.url).getOrElse("")))
-                    val strWriter = new StringWriter
-                    try {
-                        model.write(strWriter, "RDF/XML-ABBREV")
-                        Some(strWriter.toString)
-                    } catch {
-                        case cece: CannotEncodeCharacterException => throw new RuntimeException("Failed creating model for " + json, cece)
-                    } finally {
-                        strWriter.close()
-                    }
+            import collection.JavaConversions._
+            model.setNsPrefixes(Map(
+                "foaf" -> foaf,
+                "sioc" -> sioc,
+                "opo" -> opo,
+                "sonar" -> sonar): java.util.Map[String, String]
+            )
+
+            val resource = model.createResource()
+                    .addProperty(RDF.`type`, FOAF.Person)
+                    .addProperty(ResourceFactory.createProperty(foaf + "account"), model.createResource(sioc + serviceProfile.serviceType + ":" + serviceProfile.userId))
+                    .addProperty(RDF.`type`, ResourceFactory.createProperty(sioc + "UserAccount"))
+            // hacky
+            val dateFormat = new SimpleDateFormat("MM-dd")
+            Map(
+                FOAF.name -> serviceProfile.fullName,
+                FOAF.gender -> serviceProfile.gender.name(),
+                FOAF.birthday -> ??(serviceProfile.birthday).map(dateFormat.format).orNull,
+                FOAF.accountName -> serviceProfile.userId,
+                FOAF.mbox -> serviceProfile.aliases.email,
+                FOAF.homepage -> serviceProfile.url,
+                model.createProperty(foaf + "twitterId") -> serviceProfile.aliases.twitter,
+                model.createProperty(foaf + "facebookId") -> serviceProfile.aliases.facebook
+            ) foreach {
+                case (p, value) if value != null =>
+                    resource.addProperty(p, value)
             }
+            val update = updateFromStatements(model.listStatements)
+            val ds = new DatasetImpl(model)
+            ds.asDatasetGraph().find()
+            UpdateExecutionFactory.createRemote(update, "http://ec2-107-22-231-224.compute-1.amazonaws.com:3030/data/update").execute()
+            /*
+                       val strWriter = new StringWriter
 
+           try {
+                model.write(strWriter, "RDF/XML-ABBREV")
+                Some(strWriter.toString)
+            } catch {
+                case cece: CannotEncodeCharacterException => throw new RuntimeException("Failed creating model for " + serviceProfile, cece)
+            } finally {
+                strWriter.close()
+            }*/
+            ""
         }
     }
     models
             .write(profileRdf)
 
-    models
-            .write(profileRdfSequence)
 
+    def updateFromStatements(stmts: StmtIterator) = {
+        val update = new UpdateDeleteInsert
+
+        stmts foreach {
+            stmt =>
+                update.getInsertAcc.addTriple(new com.hp.hpl.jena.graph.Triple(stmt.getSubject, stmt.getPredicate, stmt.getObject))
+        }
+        update
+    }
 }
 
