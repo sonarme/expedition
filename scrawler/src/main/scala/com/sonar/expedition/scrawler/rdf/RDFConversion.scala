27cff9df20c2ed2c3475d1a4d8a04b11a0555e2e
Roger Chang <roger@sonar.me>
2012-11-05 16:53:33 -0500

    add profileToRdf using elmo

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/rdf/RDFConversion.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/rdf/RDFConversion.scala
@@ -9,6 +9,15 @@ import com.sonar.expedition.scrawler.util.RdfFormat
 import com.hp.hpl.jena.shared.CannotEncodeCharacterException
 import scala.Predef._
 import collection.JavaConversions._
+import org.openrdf.elmo.ElmoModule
+import org.openrdf.elmo.sesame.SesameManagerFactory
+import org.openrdf.rio.RDFFormat
+import javax.xml.namespace.QName
+import com.sonar.dossier.dto.ServiceProfileDTO
+import com.sonar.ontologies.foaf.{Image, Person}
+import org.openrdf.rio.ntriples.NTriplesWriter
+import org.openrdf.rio.rdfxml.RDFXMLWriter
+import org.openrdf.rio.turtle.TurtleWriter
 
 trait RDFConversion {
     def placeToRDF(serType: String, venId: String, venName: String, venAddress: String, lat: Double, lng: Double, rdfFormat: RdfFormat.Value = RdfFormat.Ntriple) = {
@@ -41,5 +50,51 @@ trait RDFConversion {
             strWriter.close()
         }
 
+    }
+
+    def profileToRDF(serviceProfile: ServiceProfileDTO) = {
+        val module = new ElmoModule()
+        module.addConcept(classOf[Person])
+        module.addConcept(classOf[Image])
+        val factory = new SesameManagerFactory(module)
+        val manager = factory.createElmoManager()
+
+        val id = new QName("http://xmlns.com/foaf/0.1/Person/", serviceProfile.serviceType.toString + ":" + serviceProfile.userId)
+        val person = manager.designate(id, classOf[Person])
+
+        person.setFoafName(Set[String](serviceProfile.fullName))
+
+
+        if (serviceProfile.fullName != null) {
+            person.setFoafName(Set[String](serviceProfile.fullName))
+        }
+        if (serviceProfile.gender != null) {
+            person.setFoafGender(serviceProfile.gender.toString)
+        }
+        if (serviceProfile.birthday != null) {
+            person.setFoafBirthday(serviceProfile.birthday)
+        }
+        if (serviceProfile.aliases != null && serviceProfile.aliases.email != null) {
+            person.setFoafMbox(Set[String](serviceProfile.aliases.email))
+        }
+        if (serviceProfile.photoUrl != null) {
+            val photoId = new QName(serviceProfile.photoUrl)
+            val photo = manager.designate(photoId, classOf[Image])
+            person.setFoafDepiction(Set[Image](photo))
+        }
+
+//        val file = new File("/Users/rogchang/Desktop/test.rdf")
+//        val writer = new FileWriter(file)
+        val writer = new StringWriter()
+        try {
+            //        manager.getConnection.export(new NTriplesWriter(writer))
+            manager.getConnection.export(new RDFXMLWriter(writer))
+            //        manager.getConnection.export(new TurtleWriter(writer))
+            writer.toString
+        } catch {
+            case e: Exception => throw new RuntimeException("Failed writing rdf", e)
+        } finally {
+            writer.close()
+        }
     }
 }
--- scrawler/src/test/scala/com/sonar/expedition/scrawler/rdf/RDFConversionTest.scala
+++ scrawler/src/test/scala/com/sonar/expedition/scrawler/rdf/RDFConversionTest.scala
@@ -2,8 +2,9 @@ package com.sonar.expedition.scrawler.rdf
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
-import com.sonar.dossier.dto.ServiceType
+import com.sonar.dossier.dto.{Aliases, Gender, ServiceProfileDTO, ServiceType}
 import com.sonar.expedition.scrawler.util.RdfFormat
+import java.util.Date
 
 class RDFConversionTest extends FlatSpec with ShouldMatchers with RDFConversion {
 
@@ -12,5 +13,16 @@ class RDFConversionTest extends FlatSpec with ShouldMatchers with RDFConversion 
         assert(rdf contains """<lgdo:Amenity rdf:about="http://sonar.me/venue#foursquare_123ab">""")
     }
 
+    "profile tuple" should "be written into RDF" in {
+        val serviceProfile = new ServiceProfileDTO(serviceType = ServiceType.foursquare, userId = "rogerchang")
+        serviceProfile.gender = Gender.male
+        serviceProfile.fullName = "Roger Chang"
+        serviceProfile.birthday = new Date
+        serviceProfile.photoUrl = "http://www.sonar.me/somepic.jpg"
+        serviceProfile.aliases = new Aliases(username = "rog", email = "roger@sonar.me", facebook = "fb1234", twitter = "tw1234")
+        val rdf = profileToRDF(serviceProfile)
+        println(rdf)
+        assert(rdf.nonEmpty)
+    }
 }
 
