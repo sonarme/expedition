03c38a722a91eee81726c6bb1d93581b81e44f54
Roger Chang <roger@sonar.me>
2012-11-08 16:00:06 -0500

    add SitemapLinksJob to write sitemap links to tuple

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/crawler/Sites.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/crawler/Sites.scala
@@ -1,10 +1,11 @@
 package com.sonar.expedition.scrawler.crawler
 
 object Sites {
-    val Yelp = "yelp.com"
     val CitySearch = "citysearch.com"
     val Foursquare = "foursquare.com"
     val Facebook = "facebook.com"
-    val Twitter = "twitter.com"
     val LivingSocial = "livingsocial.com"
+    val Meetup = "meetup.com"
+    val Twitter = "twitter.com"
+    val Yelp = "yelp.com"
 }
