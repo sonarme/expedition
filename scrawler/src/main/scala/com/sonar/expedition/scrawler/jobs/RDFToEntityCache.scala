b8a56312ea788fe49a9db2a351ca0557e648eb16
Ben Kempe <ben@k2medialabs.com>
2012-12-11 21:34:41 -0500

    silk loader

---
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/RDFToEntityCache.scala
@@ -0,0 +1,102 @@
+package com.sonar.expedition.scrawler.jobs
+
+import org.apache.hadoop.fs.{Path, FileSystem}
+import de.fuberlin.wiwiss.silk.config.LinkingConfig
+import com.twitter.scalding._
+import de.fuberlin.wiwiss.silk.config.LinkingConfig
+import de.fuberlin.wiwiss.silk.config.LinkSpecification
+import de.fuberlin.wiwiss.silk.LoadTask
+import de.fuberlin.wiwiss.silk.entity.{Entity, EntityDescription}
+import de.fuberlin.wiwiss.silk.util.DPair
+import de.fuberlin.wiwiss.silk.plugins.jena.{RdfDataSource, ModelDataSource, JenaPlugins, HadoopDataSource}
+import de.fuberlin.wiwiss.silk.datasource.{Source, DataSource}
+import de.fuberlin.wiwiss.silk.hadoop.impl.HadoopEntityCache
+import com.hp.hpl.jena.rdf.model.ModelFactory
+import java.io.{StringReader, InputStreamReader, BufferedReader}
+import de.fuberlin.wiwiss.silk.util.sparql.EntityRetriever
+import java.util.logging.Level
+import org.apache.hadoop
+import grizzled.slf4j.Logging
+import de.fuberlin.wiwiss.silk.plugins.Plugins
+import de.fuberlin.wiwiss.silk.plugins.jena.RdfDataSource
+import scala.Some
+import com.twitter.scalding.TextLine
+
+
+class RDFToEntityCache(args: Args) extends Job(args) with Logging {
+    val silkConfigPath = args("silkConfigPath")
+    val entityCachePathArg = args("entityCachePath")
+    val linkSpecArg = args.optional("linkSpec")
+    val buckets = 100
+
+
+    TextLine(args("rdf")).read
+            // create buckets for the n-triples by subject (doesn't work for all sparql queries)
+            .flatMap('line -> 'hashBucket) {
+        line: String =>
+            if (line.isEmpty) None
+            else {
+                val Array(subject, rest) = line.split(" ", 2)
+                Some(subject.hashCode % buckets)
+            }
+    }
+            // bucketize
+            .groupBy('hashBucket) {
+        _.mapList('line -> '__dummy__) {
+            lines: List[String] =>
+
+            // need to load silk stuff here, because some of it isn't serializable
+                Plugins.register()
+                JenaPlugins.register()
+                DataSource.register(classOf[RdfDataSource])
+                //Load the configuration
+                val silkConfig = {
+                    val filePath = new Path(silkConfigPath)
+                    val configFS = FileSystem.get(filePath.toUri, new hadoop.conf.Configuration)
+                    val stream = configFS.open(filePath)
+                    try {
+                        LinkingConfig.load(stream)
+                    } finally {
+                        stream.close()
+                        configFS.close()
+                    }
+                }
+                val linkSpec = (linkSpecArg match {
+                    case Some(spec) => silkConfig.linkSpec(spec) :: Nil
+                    case None => silkConfig.linkSpecs
+                }).head
+                val sources = linkSpec.datasets.map(_.sourceId).map(silkConfig.source(_))
+                val entityDesc = linkSpec.entityDescriptions
+
+                val entityCachePath = new Path(entityCachePathArg)
+                val cacheFS = FileSystem.get(entityCachePath.toUri, new hadoop.conf.Configuration)
+                val caches = DPair(
+                    new HadoopEntityCache(entityDesc.source, linkSpec.rule.index(_), cacheFS, entityCachePath.suffix("/source/" + linkSpec.id + "/"), silkConfig.runtime),
+                    new HadoopEntityCache(entityDesc.target, linkSpec.rule.index(_), cacheFS, entityCachePath.suffix("/target/" + linkSpec.id + "/"), silkConfig.runtime)
+                )
+                for (selectSource <- Seq(true, false)) {
+
+                    val entityCache = caches.select(selectSource)
+                    val source = sources.select(selectSource) match {
+                        case Source(id, rdfDataSource: RdfDataSource) => Source(id, new RdfDataSource(lines.mkString("\n"), rdfDataSource.format))
+                        case _ => throw new RuntimeException("Only supports RdfDataSource")
+                    }
+
+                    try {
+                        info("Loading entities of dataset")
+
+                        entityCache.clear()
+                        entityCache.write(source.retrieve(entityCache.entityDesc))
+                        entityCache.close()
+                    } catch {
+                        case ex: Exception =>
+                            warn("Error loading resources", ex)
+
+                    }
+                }
+                true
+        }
+    }.write(NullSource)
+
+
+}
