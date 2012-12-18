918238fcc152c555cb2691146be0b0bdb4866136
Ben Kempe <ben@k2medialabs.com>
2012-12-18 17:25:48 -0500

    place inference

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/checkins/CheckinInference.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/checkins/CheckinInference.scala
@@ -8,7 +8,7 @@ import de.fuberlin.wiwiss.silk.plugins.metric.GeographicDistanceMetric
 import cascading.pipe.Pipe
 import com.sonar.expedition.scrawler.pipes.ScaldingImplicits
 import de.fuberlin.wiwiss.silk.linkagerule.similarity.{SimpleDistanceMeasure, DistanceMeasure}
-import cascading.tuple.Fields
+import cascading.tuple.{Tuple, Fields}
 import com.sonar.dossier.dto.GeodataDTO
 
 trait CheckinInference extends ScaldingImplicits {
@@ -26,22 +26,25 @@ trait CheckinInference extends ScaldingImplicits {
             measure.indexValue(location.canonicalId, threshold).flatten
     }
 
-    def matchGeo(smaller: (Pipe, Fields, Fields, Fields), larger: (Pipe, Fields, Fields, Fields), distance: Fields, measure: SimpleDistanceMeasure = GeographicDistanceMetric("m"), threshold: Int = 50, top: Int = 3) = {
-        val (smallerPipe, smallerId, smallerBlockId, smallerLocation) = smaller
-        val (largerPipe, largerId, largerBlockId, largerLocation) = larger
+    def matchGeo(smaller: (Pipe, Fields, Fields), larger: (Pipe, Fields, Fields), distance: Fields, metaFields: Fields, measure: SimpleDistanceMeasure = GeographicDistanceMetric("m"), threshold: Int = 50, top: Int = 3) = {
+        val (smallerPipe, smallerBlockId, smallerLocation) = smaller
+        val (largerPipe, largerBlockId, largerLocation) = larger
+        val blockIds = Fields.merge(smallerBlockId, largerBlockId)
+        val groupFields = 'indexEl append blockIds
+        val distanceAndMetaFields = distance append metaFields subtract blockIds
         addIndex(smallerPipe, smallerLocation, measure, threshold)
-                .joinWithLarger((smallerBlockId, 'indexEl) ->(largerBlockId, 'indexEl), addIndex(largerPipe, largerLocation, measure, threshold))
+                .joinWithLarger(('indexEl append smallerBlockId) -> ('indexEl append largerBlockId), addIndex(largerPipe, largerLocation, measure, threshold))
                 .flatMap((smallerLocation, largerLocation) -> distance) {
             in: (GeodataDTO, GeodataDTO) =>
                 val (locationSmaller, locationLarger) = in
                 val distance = measure.evaluate(locationSmaller.canonicalId, locationLarger.canonicalId)
                 if (distance > threshold) None else Some(distance)
-        }.groupBy(smallerBlockId, largerBlockId, 'indexEl) {
-            _.sortWithTake[(String, String, Double)]((smallerId, largerId, distance) -> 'topEls, top) {
-                (left: (String, String, Double), right: (String, String, Double)) =>
-                    left._3 > right._3
+        }.groupBy(groupFields) {
+            _.sortWithTake[Tuple](distanceAndMetaFields -> 'topEls, top) {
+                (left: Tuple, right: Tuple) =>
+                    left.getDouble(0) > right.getDouble(0)
             }
-        }.discard('indexEl).flatten[(String, String, Double)]('topEls ->(smallerId, largerId, distance))
+        }.discard('indexEl).flatten[Tuple]('topEls -> distanceAndMetaFields)
     }
 
     def isWeekDay(ldt: LocalDateTime) = WeekDays(ldt.getDayOfWeek)
--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/behavior/PlaceInferenceJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/behavior/PlaceInferenceJob.scala
@@ -1,23 +1,36 @@
 package com.sonar.expedition.scrawler.jobs.behavior
 
-import com.sonar.dossier.dto.{ServiceType, GeodataDTO, CheckinDTO}
-import com.twitter.scalding.{Tsv, SequenceFile, Job, Args}
+import com.sonar.dossier.dto._
+import com.twitter.scalding._
 import com.sonar.expedition.scrawler.util.{LocationClusterer, Tuples}
-import com.sonar.dossier.Normalizers
+import com.sonar.dossier.{dto, Normalizers}
 import com.sonar.expedition.scrawler.checkins.CheckinInference
 import com.sonar.expedition.scrawler.util.CommonFunctions._
-import org.joda.time.{LocalTime, LocalDateTime}
-import de.fuberlin.wiwiss.silk.plugins.metric.GeographicDistanceMetric
+import com.sonar.expedition.scrawler.util.CommonFunctions.Segment
+import com.twitter.scalding.IterableSource
+import com.sonar.dossier.dto.GeodataDTO
+import org.scala_tools.time.Imports._
+import cascading.tuple.Fields
 
 class PlaceInferenceJob(args: Args) extends Job(args) with Normalizers with CheckinInference {
-    val argsCheckin = args("checkinsIn")
-    val argsVenues = args("venuesIn")
-    val argsStats = args("statsOut")
-    val argsPlaceInference = args("placeInferenceOut")
     val segments = Seq(0 -> 7, 6 -> 11, 11 -> 14, 13 -> 17, 16 -> 20, 19 -> 0) map {
         case (fromHr, toHr) => Segment(from = new LocalTime(fromHr, 0, 0), to = new LocalTime(toHr, 0, 0), name = toHr)
     }
-    val checkinsPipe = SequenceFile(argsCheckin, Tuples.CheckinIdDTO).read.flatMapTo(('checkinDto) ->('checkinId, 'userGoldenId, 'serviceType, 'canonicalVenueId, 'location, 'timeSegment)) {
+    //val checkinSource = SequenceFile(args("checkinsIn"), Tuples.CheckinIdDTO)
+    val checkinSource = IterableSource(Seq(
+        ("test1", dto.CheckinDTO(ServiceType.foursquare,
+            "123",
+            GeodataDTO(40.0, -74.0),
+            DateTime.now,
+            "ben123",
+
+            Some(ServiceVenueDTO(ServiceType.foursquare, "gg", "G&G", location = LocationDTO(GeodataDTO(40.0, -74.0), "x")))
+        ))
+    ), Tuples.CheckinIdDTO)
+
+    //val placeInferenceOut = SequenceFile(args("placeInferenceOut"), Tuples.PlaceInference)
+    val placeInferenceOut = NullSource
+    val checkinsPipe = checkinSource.read.flatMapTo(('checkinDto) ->('checkinId, 'userGoldenId, 'serviceType, 'canonicalVenueId, 'location, 'timeSegment)) {
         dto: CheckinDTO =>
             val ldt = localDateTime(dto.latitude, dto.longitude, dto.checkinTime.toDate)
             val weekDay = isWeekDay(ldt)
@@ -32,10 +45,10 @@ class PlaceInferenceJob(args: Args) extends Job(args) with Normalizers with Chec
         in: (ServiceType, String) =>
             val (serviceType, canonicalVenueId) = in
             canonicalVenueId != null && serviceType == ServiceType.foursquare
-    }.groupBy('userGoldenId, 'canonicalVenueId, 'location, 'timeSegment) {
+    }.rename('location, 'venueLocation).groupBy('userGoldenId, 'canonicalVenueId, 'venueLocation, 'timeSegment) {
         _.size('venueUserPopularity)
     }
-    val venuePopularity = venueUserPopularity.groupBy('canonicalVenueId, 'location, 'timeSegment) {
+    val venuePopularity = venueUserPopularity.groupBy('canonicalVenueId, 'venueLocation, 'timeSegment) {
         _.sum('venueUserPopularity -> 'venuePopularity)
     }
 
@@ -56,23 +69,19 @@ class PlaceInferenceJob(args: Args) extends Job(args) with Normalizers with Chec
                         dsValues.map(checkinMap).map(_ -> geodata)
                 }.toSeq
         }
-    }.flatten[(String, GeodataDTO)]('clusterAverages ->('checkinId, 'location)).discard('clusterAverages)
+    }.flatten[(String, GeodataDTO)]('clusterAverages ->('checkinId, 'location)).discard('clusterAverages, 'timeSegment)
 
-    val locations = clusterAveragesPipe.unique('location)
-    val withUserPopularity = matchGeo((venueUserPopularity, 'venueId, 'userGoldenId, 'location), (clusterAveragesPipe, 'checkinId, 'userGoldenId, 'location), 'score).map('score -> 'score) {
+    val metaFields: Fields = ('canonicalVenueId, 'checkinId, 'userGoldenId, 'timeSegment)
+    val withUserPopularity = matchGeo((venueUserPopularity, 'userGoldenId, 'venueLocation), (clusterAveragesPipe, 'userGoldenId, 'location), 'score, metaFields).map('score -> 'score) {
         distance: Double => distance / 10.0
     }
-    val withVenuePopularity = matchGeo((venuePopularity.map(() -> 'blockId) {
-        _: Unit => ""
-    }, 'venueId, 'blockId, 'location), (clusterAveragesPipe.map(() -> 'blockId) {
-        _: Unit => ""
-    }, 'checkinId, 'blockId, 'location), 'score)
-    val topScores = (withUserPopularity.project('blockId, 'idSmaller, 'idLarger, 'distance) ++ withVenuePopularity).groupBy('userGoldenId, 'checkinId, 'timeSegment) {
-        _.sortWithTake(('venueId, 'score) -> 'topScores, 3) {
+    val withVenuePopularity = matchGeo((venuePopularity, Fields.NONE, 'venueLocation), (clusterAveragesPipe, Fields.NONE, 'location), 'score, metaFields)
+    val topScores = (withUserPopularity.project('userGoldenId, 'checkinId, 'timeSegment, 'canonicalVenueId, 'score) ++ withVenuePopularity.project('userGoldenId, 'checkinId, 'timeSegment, 'canonicalVenueId, 'score)).groupBy('userGoldenId, 'checkinId, 'timeSegment) {
+        _.sortWithTake(('canonicalVenueId, 'score) -> 'topScores, 3) {
             (left: (String, Double), right: (String, Double)) => left._2 > right._2
         }
-    }.flatten[(String, Double)]('venueId, 'score)
-    topScores.write(SequenceFile(argsPlaceInference, Tuples.PlaceInference))
+    }.flatten[(String, Double)]('topScores ->('canonicalVenueId, 'score))
+    topScores.write(placeInferenceOut)
 
 
 }
