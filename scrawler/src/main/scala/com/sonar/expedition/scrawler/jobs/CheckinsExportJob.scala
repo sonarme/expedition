44f74cfebb4311ca37328b2b002c4e3b76240bf9
Ben Kempe <ben@k2medialabs.com>
2013-01-08 13:45:47 -0500

    new checkins export

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/CheckinsExportJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/CheckinsExportJob.scala
@@ -2,10 +2,63 @@ package com.sonar.expedition.scrawler.jobs
 
 import com.twitter.scalding.{SequenceFile, Args, Job}
 import cascading.tuple.Fields
+import com.sonar.scalding.cassandra.{NarrowRowScheme, CassandraSource}
+import com.sonar.expedition.scrawler.util.CommonFunctions._
+import com.twitter.scalding.SequenceFile
+import scala.Some
+import com.sonar.expedition.scrawler.util.{CommonFunctions, Tuples}
+import java.nio.ByteBuffer
+import me.prettyprint.cassandra.serializers._
+import com.sonar.dossier.dto.CheckinDTO
+import com.sonar.dossier.cassandra.converters.ServiceTypeConverter
+import me.prettyprint.hom.converters.JodaTimeHectorConverter
+import com.sonar.scalding.cassandra.CassandraSource
+import com.twitter.scalding.SequenceFile
+import com.sonar.scalding.cassandra.NarrowRowScheme
 
 class CheckinsExportJob(args: Args) extends Job(args) with CheckinSource {
-    val (checkins, _) = checkinSource(args, false, false)
-    val checkinsOutput = args("output")
+    val checkins = CassandraSource(
+        rpcHost = args("rpcHost"),
+        additionalConfig = ppmap(args),
+        keyspaceName = "dossier",
+        columnFamilyName = "Checkin",
+        scheme = NarrowRowScheme(keyField = 'idB,
+            valueFields = ('viewingUserSonarIdB, 'checkinTimeB, 'serviceTypeB, 'serviceProfileIdB, 'latitudeB, 'longitudeB, 'venueNameB, 'venueAddressB, 'venueSiteUrlB, 'venueIdB, 'messageB, 'serviceCheckinIdB, 'notPublicB, 'clientIdB, 'rawB, 'horizontalAccuracyB, 'verticalAccuracyB, 'batteryLevelB, 'courseB, 'speedB, 'calculatedSpeedB),
+            columnNames = List("viewingUserSonarId", "checkinTime", "serviceType", "serviceProfileId", "latitude", "longitude", "venueName", "venueAddress", "venueSiteUrl", "venueId", "message", "serviceCheckinId", "notPublic", "clientId", "raw", "horizontalAccuracy", "verticalAccuracy", "batteryLevel", "course", "speed", "calculatedSpeed"))
+    ).flatMapTo(('idB, 'viewingUserSonarIdB, 'checkinTimeB, 'serviceTypeB, 'serviceProfileIdB, 'latitudeB, 'longitudeB, 'venueNameB, 'venueAddressB, 'venueSiteUrlB, 'venueIdB, 'messageB, 'serviceCheckinIdB, 'notPublicB, 'clientIdB, 'rawB, 'horizontalAccuracyB, 'verticalAccuracyB, 'batteryLevelB, 'courseB, 'speedB, 'calculatedSpeedB) -> Tuples.CheckinIdDTO) {
+        in: (ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer,
+                ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer,
+                ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer,
+                ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer) => {
+            val (id, viewingUserSonarId, checkinTime, serviceType, serviceProfileId, latitude, longitude, venueName, venueAddress, venueSiteUrl, venueId, message, serviceCheckinId, notPublic, clientId, raw, horizontalAccuracy, verticalAccuracy, batteryLevel, course, speed, calculatedSpeed) = in
+            val checkin = new CheckinDTO
+            checkin.id = StringSerializer.get.fromByteBuffer(id)
+            checkin.viewingUserSonarId = StringSerializer.get.fromByteBuffer(viewingUserSonarId)
+            checkin.checkinTime = (new JodaTimeHectorConverter) convertCassTypeToObjType(null, BytesArraySerializer.get.fromByteBuffer(checkinTime))
+            checkin.serviceType = (new ServiceTypeConverter) convertCassTypeToObjType(null, BytesArraySerializer.get.fromByteBuffer(serviceType))
+            checkin.serviceProfileId = StringSerializer.get.fromByteBuffer(serviceProfileId)
+            checkin.latitude = DoubleSerializer.get.fromByteBuffer(latitude)
+            checkin.longitude = DoubleSerializer.get.fromByteBuffer(longitude)
+            checkin.venueName = StringSerializer.get.fromByteBuffer(venueName)
+            checkin.venueAddress = StringSerializer.get.fromByteBuffer(venueAddress)
+            checkin.venueSiteUrl = StringSerializer.get.fromByteBuffer(venueSiteUrl)
+            checkin.venueId = StringSerializer.get.fromByteBuffer(venueId)
+            checkin.message = StringSerializer.get.fromByteBuffer(message)
+            checkin.serviceCheckinId = StringSerializer.get.fromByteBuffer(serviceCheckinId)
+            checkin.notPublic = BooleanSerializer.get.fromByteBuffer(notPublic)
+            checkin.clientId = StringSerializer.get.fromByteBuffer(clientId)
+            checkin.raw = StringSerializer.get.fromByteBuffer(raw)
+            checkin.horizontalAccuracy = DoubleSerializer.get.fromByteBuffer(horizontalAccuracy)
+            checkin.verticalAccuracy = DoubleSerializer.get.fromByteBuffer(verticalAccuracy)
+            checkin.batteryLevel = DoubleSerializer.get.fromByteBuffer(batteryLevel)
+            checkin.course = DoubleSerializer.get.fromByteBuffer(course)
+            checkin.speed = DoubleSerializer.get.fromByteBuffer(speed)
+            checkin.calculatedSpeed = DoubleSerializer.get.fromByteBuffer(calculatedSpeed)
+            Some((checkin.id, checkin))
+        }
+    }
+
+    val checkinsOutput = args("checkinsOut")
     checkins.write(SequenceFile(checkinsOutput, Fields.ALL))
-            .limit(180000).write(SequenceFile(checkinsOutput + "_small", Fields.ALL))
+            .limit(18000).write(SequenceFile(checkinsOutput + "_small", Fields.ALL))
 }
