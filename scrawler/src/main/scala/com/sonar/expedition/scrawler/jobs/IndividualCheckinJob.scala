b4cbead0601246063cb14777dcb0d2f1b8b8a163
Ben Kempe <ben@k2medialabs.com>
2013-01-08 18:12:18 -0500

    job

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/IndividualCheckinJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/IndividualCheckinJob.scala
@@ -2,13 +2,16 @@ package com.sonar.expedition.scrawler.jobs
 
 import com.twitter.scalding.{SequenceFile, Args, Job}
 import com.sonar.expedition.scrawler.util
+import cascading.tuple.Fields
+import util.Tuples
+import com.sonar.dossier.dto.CheckinDTO
 
 class IndividualCheckinJob(args: Args) extends Job(args) with CheckinSource {
-    val (checkins, _) = checkinSource(args, false, false)
-    val Array(serviceType, serviceId) = args("serviceProfileId").split(':')
-    val individual = serviceType + ":" + util.CommonFunctions.hashed(serviceId)
-    checkins.filter('keyid) {
-        keyid: String => keyid == individual
-    }.write(SequenceFile(args("output")))
+    val individual = args("profileIds").split(',').toSet[String]
+    SequenceFile(args("checkinsIn"), Tuples.CheckinIdDTO).flatMapTo('checkinDto ->('lat, 'lng)) {
+        checkin: CheckinDTO =>
+            if (individual(checkin.profileId)) Some((checkin.latitude, checkin.longitude))
+            else None
+    }.write(SequenceFile(args("filteredOut")))
 }
 
