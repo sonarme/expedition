d046010a7c0cfed670e92da6086ffa49329cad9f
Ben Kempe <ben@k2medialabs.com>
2013-01-08 19:48:55 -0500

    export

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/CheckinsExportJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/CheckinsExportJob.scala
@@ -5,7 +5,7 @@ import cascading.tuple.Fields
 import com.sonar.scalding.cassandra.{NarrowRowScheme, CassandraSource}
 import com.sonar.expedition.scrawler.util.CommonFunctions._
 import com.twitter.scalding.SequenceFile
-import scala.Some
+import scala.{None, Some}
 import com.sonar.expedition.scrawler.util.{CommonFunctions, Tuples}
 import java.nio.ByteBuffer
 import me.prettyprint.cassandra.serializers._
@@ -31,35 +31,32 @@ class CheckinsExportJob(args: Args) extends Job(args) with CheckinSource {
                 ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer,
                 ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer) => {
             val (id, viewingUserSonarId, checkinTime, serviceType, serviceProfileId, latitude, longitude, venueName, venueAddress, venueSiteUrl, venueId, message, serviceCheckinId, notPublic, clientId, raw, horizontalAccuracy, verticalAccuracy, batteryLevel, course, speed, calculatedSpeed) = in
+            val checkin = new CheckinDTO
+            checkin.id = StringSerializer.get.fromByteBuffer(id)
+            checkin.viewingUserSonarId = StringSerializer.get.fromByteBuffer(viewingUserSonarId)
             val checkinTimeBA = BytesArraySerializer.get.fromByteBuffer(checkinTime)
             // not sure why this happens, but it does
-            if (checkinTimeBA == null) None
-            else {
-                val checkin = new CheckinDTO
-                checkin.id = StringSerializer.get.fromByteBuffer(id)
-                checkin.viewingUserSonarId = StringSerializer.get.fromByteBuffer(viewingUserSonarId)
-                checkin.checkinTime = (new JodaTimeHectorConverter) convertCassTypeToObjType(null, checkinTimeBA)
-                checkin.serviceType = ServiceType.valueOf(checkin.id.split(':')(0))
-                checkin.serviceProfileId = StringSerializer.get.fromByteBuffer(serviceProfileId)
-                checkin.latitude = DoubleSerializer.get.fromByteBuffer(latitude)
-                checkin.longitude = DoubleSerializer.get.fromByteBuffer(longitude)
-                checkin.venueName = StringSerializer.get.fromByteBuffer(venueName)
-                checkin.venueAddress = StringSerializer.get.fromByteBuffer(venueAddress)
-                checkin.venueSiteUrl = StringSerializer.get.fromByteBuffer(venueSiteUrl)
-                checkin.venueId = StringSerializer.get.fromByteBuffer(venueId)
-                checkin.message = StringSerializer.get.fromByteBuffer(message)
-                checkin.serviceCheckinId = StringSerializer.get.fromByteBuffer(serviceCheckinId)
-                checkin.notPublic = BooleanSerializer.get.fromByteBuffer(notPublic)
-                checkin.clientId = StringSerializer.get.fromByteBuffer(clientId)
-                checkin.raw = StringSerializer.get.fromByteBuffer(raw)
-                checkin.horizontalAccuracy = DoubleSerializer.get.fromByteBuffer(horizontalAccuracy)
-                checkin.verticalAccuracy = DoubleSerializer.get.fromByteBuffer(verticalAccuracy)
-                checkin.batteryLevel = DoubleSerializer.get.fromByteBuffer(batteryLevel)
-                checkin.course = DoubleSerializer.get.fromByteBuffer(course)
-                checkin.speed = DoubleSerializer.get.fromByteBuffer(speed)
-                checkin.calculatedSpeed = DoubleSerializer.get.fromByteBuffer(calculatedSpeed)
-                Some((checkin.id, checkin))
-            }
+            checkin.checkinTime = if (checkinTimeBA == null) null else (new JodaTimeHectorConverter) convertCassTypeToObjType(null, checkinTimeBA)
+            checkin.serviceType = ServiceType.valueOf(checkin.id.split(':')(0))
+            checkin.serviceProfileId = StringSerializer.get.fromByteBuffer(serviceProfileId)
+            checkin.latitude = DoubleSerializer.get.fromByteBuffer(latitude)
+            checkin.longitude = DoubleSerializer.get.fromByteBuffer(longitude)
+            checkin.venueName = StringSerializer.get.fromByteBuffer(venueName)
+            checkin.venueAddress = StringSerializer.get.fromByteBuffer(venueAddress)
+            checkin.venueSiteUrl = StringSerializer.get.fromByteBuffer(venueSiteUrl)
+            checkin.venueId = StringSerializer.get.fromByteBuffer(venueId)
+            checkin.message = StringSerializer.get.fromByteBuffer(message)
+            checkin.serviceCheckinId = StringSerializer.get.fromByteBuffer(serviceCheckinId)
+            checkin.notPublic = BooleanSerializer.get.fromByteBuffer(notPublic)
+            checkin.clientId = StringSerializer.get.fromByteBuffer(clientId)
+            checkin.raw = StringSerializer.get.fromByteBuffer(raw)
+            checkin.horizontalAccuracy = DoubleSerializer.get.fromByteBuffer(horizontalAccuracy)
+            checkin.verticalAccuracy = DoubleSerializer.get.fromByteBuffer(verticalAccuracy)
+            checkin.batteryLevel = DoubleSerializer.get.fromByteBuffer(batteryLevel)
+            checkin.course = DoubleSerializer.get.fromByteBuffer(course)
+            checkin.speed = DoubleSerializer.get.fromByteBuffer(speed)
+            checkin.calculatedSpeed = DoubleSerializer.get.fromByteBuffer(calculatedSpeed)
+            Some((checkin.id, checkin))
         }
     }
 
