89c18ec5524f64232b6fe92a25671def4b49f042
Ben Kempe <ben@k2medialabs.com>
2013-01-11 17:18:43 -0500

    allow old

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/IndividualCheckinJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/IndividualCheckinJob.scala
@@ -8,10 +8,15 @@ import com.sonar.dossier.dto.CheckinDTO
 
 class IndividualCheckinJob(args: Args) extends Job(args) with CheckinSource {
     val individual = args("profileIds").split(',').toSet[String]
-    SequenceFile(args("checkinsIn"), Tuples.CheckinIdDTO).flatMapTo('checkinDto ->('lat, 'lng, 'checkinTime)) {
-        checkin: CheckinDTO =>
-            if (individual(checkin.profileId)) Some((checkin.latitude, checkin.longitude, checkin.checkinTime.toDate))
-            else None
-    }.write(Tsv(args("filteredOut")))
+
+    val old = args.optional("old").map(_.toBoolean).getOrElse(false)
+    if (old)
+        SequenceFile(args("checkinsIn"), Tuples.Checkin).project('lat, 'lng, 'chknTime).write(Tsv(args("filteredOut")))
+    else
+        SequenceFile(args("checkinsIn"), Tuples.CheckinIdDTO).flatMapTo('checkinDto ->('lat, 'lng, 'checkinTime)) {
+            checkin: CheckinDTO =>
+                if (individual(checkin.profileId)) Some((checkin.latitude, checkin.longitude, checkin.checkinTime.toDate))
+                else None
+        }.write(Tsv(args("filteredOut")))
 }
 
