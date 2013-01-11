91c48fbc657d55da217ffb4b6e0a1048e980cb04
Ben Kempe <ben@k2medialabs.com>
2013-01-10 19:32:03 -0500

    mappers

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/OldSchoolCHeckinsExportJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/OldSchoolCHeckinsExportJob.scala
@@ -4,6 +4,11 @@ import com.twitter.scalding.{SequenceFile, Args, Job}
 import cascading.tuple.Fields
 
 class OldSchoolCheckinsExportJob(args: Args) extends Job(args) with CheckinSource {
+
+    override def config =
+           super.config ++
+                   Map("mapred.map.tasks" -> "50")
+
     val (checkins, _) = checkinSource(args, false, false)
     val checkinsOutput = args("checkinsOut")
     checkins.write(SequenceFile(checkinsOutput, Fields.ALL))
