a783e3fca7deb81ead21e17cf2864301d11f3fdc
Ben Kempe <ben@k2medialabs.com>
2013-01-14 14:58:34 -0500

    more java conversion serialization

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/DefaultJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/DefaultJob.scala
@@ -4,7 +4,7 @@ import com.twitter.scalding.{Args, Job}
 import cascading.kryo.KryoFactory
 import org.scala_tools.time.Imports._
 import de.javakaffee.kryoserializers.jodatime.JodaDateTimeSerializer
-import com.sonar.expedition.scrawler.serializer.ArrayListSerializer
+import com.sonar.expedition.scrawler.serializer.{HashMapSerializer, HashSetSerializer, ArrayListSerializer}
 
 class DefaultJob(args: Args) extends Job(args) {
 
@@ -12,6 +12,9 @@ class DefaultJob(args: Args) extends Job(args) {
         super.config ++
                 Map(KryoFactory.KRYO_REGISTRATIONS ->
                         Seq(classOf[DateTime].getCanonicalName + "," + classOf[JodaDateTimeSerializer].getCanonicalName,
-                            "scala.collection.JavaConversions$SeqWrapper" + "," + classOf[ArrayListSerializer].getCanonicalName).mkString(":"))
+                            "scala.collection.JavaConversions$SeqWrapper" + "," + classOf[ArrayListSerializer].getCanonicalName,
+                            "scala.collection.JavaConversions$SetWrapper" + "," + classOf[HashSetSerializer].getCanonicalName,
+                            "scala.collection.JavaConversions$MapWrapper" + "," + classOf[HashMapSerializer].getCanonicalName
+                        ).mkString(":"))
 
 }
--- scrawler/src/main/scala/com/sonar/expedition/scrawler/serializer/Serializers.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/serializer/Serializers.scala
@@ -1,6 +1,6 @@
 package com.sonar.expedition.scrawler.serializer
 
-import com.esotericsoftware.kryo.serializers.CollectionSerializer
+import com.esotericsoftware.kryo.serializers.{MapSerializer, CollectionSerializer}
 import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.Input
 import java.util
@@ -8,6 +8,19 @@ import java.util
 class ArrayListSerializer extends CollectionSerializer {
     override def create(kryo: Kryo, input: Input, `type`: Class[util.Collection[_]]) =
         new util.ArrayList[Any]()
+
+}
+
+class HashSetSerializer extends CollectionSerializer {
+    override def create(kryo: Kryo, input: Input, `type`: Class[util.Collection[_]]) =
+        new util.HashSet[Any]()
+
+}
+
+class HashMapSerializer extends MapSerializer {
+
+    override def create(kryo: Kryo, input: Input, `type`: Class[util.Map[_, _]]) =
+        new util.HashMap[Any, Any]()
 
 }
 
