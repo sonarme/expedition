25521ed5c48df758e8e779df2f0634931a18cd55
Ben Kempe <ben@k2medialabs.com>
2013-01-16 15:11:14 -0500

    venue reduce

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/behavior/ServiceVenueJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/behavior/ServiceVenueJob.scala
@@ -1,6 +1,6 @@
 package com.sonar.expedition.scrawler.jobs.behavior
 
-import com.sonar.dossier.dto.{ServiceType, CheckinDTO}
+import com.sonar.dossier.dto.{ServiceVenueDTO, ServiceType, CheckinDTO}
 import com.twitter.scalding.{Tsv, SequenceFile, Job, Args}
 import com.sonar.expedition.scrawler.util.Tuples
 import com.sonar.dossier.Normalizers
@@ -10,7 +10,7 @@ class ServiceVenueJob(args: Args) extends DefaultJob(args) with Normalizers {
     val argsCheckin = args("checkinsIn")
     val argsVenues = args("venuesOut")
     val argsStats = args("statsOut")
-    SequenceFile(argsCheckin, Tuples.CheckinIdDTO).read.flatMapTo(('checkinDto) ->('venueId, 'venueDto, 'nonRaw)) {
+    val venues = SequenceFile(argsCheckin, Tuples.CheckinIdDTO).read.flatMapTo(('checkinDto) ->('venueId, 'venueDto, 'raw)) {
         dto: CheckinDTO =>
             if (dto.venueId == null || dto.serviceType != ServiceType.foursquare) None
             else {
@@ -22,15 +22,20 @@ class ServiceVenueJob(args: Args) extends DefaultJob(args) with Normalizers {
                         case npe: NullPointerException => throw new RuntimeException("from raw: " + dto.raw, npe)
                     }
                 val venue = rawNormalizedVenue getOrElse dto.serviceVenue
-                Some((venue.canonicalId, venue, rawNormalizedVenue.isEmpty))
+                Some((venue.canonicalId, venue, rawNormalizedVenue.isDefined))
             }
     }.groupBy('venueId) {
         // pick the venue resulting from raw data first, because it contains categories etc.
-        _.sortBy('nonRaw).head('nonRaw, 'venueDto)
-    }.write(SequenceFile(argsVenues, Tuples.VenueIdDTO)).groupBy('nonRaw) {
+        _.reduce('raw, 'venueDto) {
+            (left: (Boolean, ServiceVenueDTO), right: (Boolean, ServiceVenueDTO)) =>
+                if (left._1) left else right
+        }
+    }
+    venues.write(SequenceFile(argsVenues, Tuples.VenueIdDTO))
+    venues.groupBy('raw) {
         // write some stats so we know how many venues from raw we have
         _.size
-    }.write(Tsv(argsStats, ('nonRaw, 'size)))
+    }.write(Tsv(argsStats, ('raw, 'size)))
 
 
 }
