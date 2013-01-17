c568ceec328273e938e82b962aca240aedb7d555
Ben Kempe <ben@k2medialabs.com>
2013-01-17 16:22:48 -0500

    profile verification

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/export/DataVerificationJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/export/DataVerificationJob.scala
@@ -3,25 +3,46 @@ package com.sonar.expedition.scrawler.jobs.export
 import com.twitter.scalding.{Tsv, Args, SequenceFile}
 import com.sonar.expedition.scrawler.util.Tuples
 import com.sonar.expedition.scrawler.jobs.{CheckinSource, DefaultJob}
+import com.sonar.dossier.dto.{ServiceProfileDTO, ServiceProfileLink}
+import com.sonar.dossier.jackson.CassandraObjectMapperJava
 
 class DataVerificationJob(args: Args) extends DefaultJob(args) with CheckinSource {
-    val checkinsNew = args("checkinsNewIn")
-    val checkinsOld = args("checkinsOldIn")
-    val checkinsOldPipe = SequenceFile(checkinsOld, Tuples.Checkin).read.mapTo(('serType, 'serCheckinID) -> 'oldCheckinId) {
-        in: (String, String) => in._1 + ":" + in._2
-    }
-    val checkinsNewPipe = SequenceFile(checkinsNew, Tuples.CheckinIdDTO).read.project('checkinId)
+    val newIn = args("newIn")
+    val oldIn = args("oldIn")
 
-    val oldStat = checkinsOldPipe.groupAll {
+    val dataType = args("dataType")
+
+
+    val oldPipe =
+        dataType match {
+            case "checkin" =>
+                SequenceFile(oldIn, Tuples.Checkin).read.mapTo(('serType, 'serCheckinID) -> 'id) {
+                    in: (String, String) => in._1 + ":" + in._2
+                }
+            case "profile" =>
+                SequenceFile(oldIn, ('userProfileId, 'serviceType, 'json)).read.mapTo('json -> 'id) {
+                    json: String =>
+                        val om = new CassandraObjectMapperJava
+                        val dto = om.readValue(json, classOf[ServiceProfileDTO])
+                        dto.link
+                }
+        }
+    val newPipe = dataType match {
+        case "checkin" =>
+            SequenceFile(newIn, Tuples.CheckinIdDTO).read.project('id)
+        case "profile" =>
+            SequenceFile(newIn, Tuples.ProfileIdDTO).read.project('profileId).rename('profileId -> 'id)
+    }
+    val oldStat = oldPipe.groupAll {
         _.size
     }.map(() -> 'statName) {
         _: Unit => "old"
     }.project('statName, 'size)
-    val newStat = checkinsOldPipe.joinWithLarger('oldCheckinId -> 'checkinId, checkinsNewPipe).groupAll {
+    val newStat = oldPipe.joinWithLarger('id -> 'id, newPipe).groupAll {
         _.size
     }.map(() -> 'statName) {
         _: Unit => "new"
     }.project('statName, 'size)
-    (oldStat ++ newStat).write(Tsv(checkinsNew + "_compare_stats", ('statName, 'size)))
+    (oldStat ++ newStat).write(Tsv(newIn + "_compare_stats", ('statName, 'size)))
 
 }
