8fe4ee6c60f8f433e1465cfd70c4fa94f363bb2e
Ben Kempe <ben@k2medialabs.com>
2013-01-17 19:06:37 -0500

    deprecate

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/util/Tuples.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/util/Tuples.scala
@@ -4,12 +4,14 @@ import com.twitter.scalding.{FieldConversions, TupleConversions}
 
 object Tuples extends TupleConversions {
     val Place = ('serType, 'venId, 'venName, 'venAddress, 'lat, 'lng)
+    @deprecated
     val Profile = ('profileId, 'profile)
     val SonarFriend = ('sonarId, 'serviceType, 'serviceProfileId)
     val PlaceInference = ('userGoldenId, 'location, 'numVisits, 'canonicalVenueId, 'score, 'timeSegment)
     val CheckinIdDTO = ('checkinId, 'checkinDto)
     val VenueIdDTO = ('venueId, 'venueDto)
     val ProfileIdDTO = ('profileId, 'profileDto, 'serviceType)
+    @deprecated
     val Checkin = ('serType, 'serProfileID, 'serCheckinID, 'venId, 'venName, 'venAddress, 'chknTime, 'lat, 'lng, 'msg)
     val Correlation = ('correlationId, 'correlationSPL)
     val Matrix = ('x, 'y, 'val)
