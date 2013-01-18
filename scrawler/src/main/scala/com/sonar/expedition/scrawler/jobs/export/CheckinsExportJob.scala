76bb67a81c27b9b9a9728279212ea2d67bd54069
Ben Kempe <ben@k2medialabs.com>
2013-01-18 11:36:43 -0500

    fix

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/export/CheckinsExportJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/export/CheckinsExportJob.scala
@@ -1,7 +1,7 @@
 package com.sonar.expedition.scrawler.jobs.export
 
 import com.twitter.scalding._
-import cascading.tuple.Fields
+import cascading.tuple.{Tuple, Fields}
 import com.sonar.scalding.cassandra.{NarrowRowScheme, CassandraSource}
 import com.sonar.expedition.scrawler.util.CommonFunctions._
 import scala.{None, Some}
@@ -25,48 +25,52 @@ import com.sonar.expedition.scrawler.jobs.{CheckinSource, DefaultJob}
 
 class CheckinsExportJob(args: Args) extends DefaultJob(args) with CheckinSource {
 
+    class TupleWrapper(val tuple: Tuple) {
+        def apply(idx: Int) = tuple.getObject(idx).asInstanceOf[ByteBuffer]
+    }
+
     val checkins = CassandraSource(
         rpcHost = args("rpcHost"),
-        additionalConfig = ppmap(args) ++ Map("cassandra.range.batch.size" -> "2048"),
+        additionalConfig = ppmap(args),
         keyspaceName = "dossier",
         columnFamilyName = "Checkin",
         scheme = NarrowRowScheme(keyField = 'idB,
-            valueFields = ('viewingUserSonarIdB, 'checkinTimeB, 'serviceTypeB, 'serviceProfileIdB, 'latitudeB, 'longitudeB, 'venueNameB, 'venueAddressB, 'venueSiteUrlB, 'venueIdB, 'messageB, 'serviceCheckinIdB, 'notPublicB, 'clientIdB, 'rawB, 'horizontalAccuracyB, 'verticalAccuracyB, 'batteryLevelB, 'courseB, 'speedB, 'calculatedSpeedB),
-            columnNames = List("viewingUserSonarId", "checkinTime", "serviceType", "serviceProfileId", "latitude", "longitude", "venueName", "venueAddress", "venueSiteUrl", "venueId", "message", "serviceCheckinId", "notPublic", "clientId", "raw", "horizontalAccuracy", "verticalAccuracy", "batteryLevel", "course", "speed", "calculatedSpeed"))
-    ).flatMapTo(('idB, 'viewingUserSonarIdB, 'checkinTimeB, 'serviceTypeB, 'serviceProfileIdB, 'latitudeB, 'longitudeB, 'venueNameB, 'venueAddressB, 'venueSiteUrlB, 'venueIdB, 'messageB, 'serviceCheckinIdB, 'notPublicB, 'clientIdB, 'rawB, 'horizontalAccuracyB, 'verticalAccuracyB, 'batteryLevelB, 'courseB, 'speedB, 'calculatedSpeedB) -> (Tuples.CheckinIdDTO append('hasIp, 'serviceType))) {
-        in: (ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer,
-                ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer,
-                ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer,
-                ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer, ByteBuffer) => {
-            val (id, viewingUserSonarId, checkinTime, serviceType, serviceProfileId, latitude, longitude, venueName, venueAddress, venueSiteUrl, venueId, message, serviceCheckinId, notPublic, clientId, raw, horizontalAccuracy, verticalAccuracy, batteryLevel, course, speed, calculatedSpeed) = in
+            valueFields = ('viewingUserSonarIdB, 'checkinTimeB, 'serviceTypeB, 'serviceProfileIdB, 'latitudeB, 'longitudeB, 'venueNameB, 'venueAddressB, 'venueSiteUrlB, 'venueIdB, 'messageB, 'serviceCheckinIdB, 'notPublicB, 'clientIdB, 'rawB, 'horizontalAccuracyB, 'verticalAccuracyB, 'batteryLevelB, 'courseB, 'speedB, 'calculatedSpeedB, 'ipB),
+            columnNames = List("viewingUserSonarId", "checkinTime", "serviceType", "serviceProfileId", "latitude", "longitude", "venueName", "venueAddress", "venueSiteUrl", "venueId", "message", "serviceCheckinId", "notPublic", "clientId", "raw", "horizontalAccuracy", "verticalAccuracy", "batteryLevel", "course", "speed", "calculatedSpeed", "ipB"))
+    ).flatMapTo(Fields.ALL -> (Tuples.CheckinIdDTO append('hasIp, 'serviceType))) {
+        in: Tuple => {
+            val tuple = new TupleWrapper(in)
+            val latitude = tuple(5)
+            val longitude = tuple(6)
             // not sure why this happens, but it does
             if (latitude == null || longitude == null) None
             else {
                 val checkin = new CheckinDTO
-                checkin.id = StringSerializer.get.fromByteBuffer(id)
-                checkin.viewingUserSonarId = StringSerializer.get.fromByteBuffer(viewingUserSonarId)
-                val checkinTimeBA = BytesArraySerializer.get.fromByteBuffer(checkinTime)
+                checkin.id = StringSerializer.get.fromByteBuffer(tuple(0))
+                checkin.viewingUserSonarId = StringSerializer.get.fromByteBuffer(tuple(1))
+                val checkinTimeBA = BytesArraySerializer.get.fromByteBuffer(tuple(2))
                 // not sure why this happens, but it does
                 checkin.checkinTime = if (checkinTimeBA == null) null else (new JodaTimeHectorConverter) convertCassTypeToObjType(null, checkinTimeBA)
                 checkin.serviceType = ServiceType.valueOf(checkin.id.split(':')(0))
-                checkin.serviceProfileId = StringSerializer.get.fromByteBuffer(serviceProfileId)
+                checkin.serviceProfileId = StringSerializer.get.fromByteBuffer(tuple(4))
                 checkin.latitude = DoubleSerializer.get.fromByteBuffer(latitude)
                 checkin.longitude = DoubleSerializer.get.fromByteBuffer(longitude)
-                checkin.venueName = StringSerializer.get.fromByteBuffer(venueName)
-                checkin.venueAddress = StringSerializer.get.fromByteBuffer(venueAddress)
-                checkin.venueSiteUrl = StringSerializer.get.fromByteBuffer(venueSiteUrl)
-                checkin.venueId = StringSerializer.get.fromByteBuffer(venueId)
-                checkin.message = StringSerializer.get.fromByteBuffer(message)
-                checkin.serviceCheckinId = StringSerializer.get.fromByteBuffer(serviceCheckinId)
-                checkin.notPublic = BooleanSerializer.get.fromByteBuffer(notPublic)
-                checkin.clientId = StringSerializer.get.fromByteBuffer(clientId)
-                checkin.raw = StringSerializer.get.fromByteBuffer(raw)
-                checkin.horizontalAccuracy = DoubleSerializer.get.fromByteBuffer(horizontalAccuracy)
-                checkin.verticalAccuracy = DoubleSerializer.get.fromByteBuffer(verticalAccuracy)
-                checkin.batteryLevel = DoubleSerializer.get.fromByteBuffer(batteryLevel)
-                checkin.course = DoubleSerializer.get.fromByteBuffer(course)
-                checkin.speed = DoubleSerializer.get.fromByteBuffer(speed)
-                checkin.calculatedSpeed = DoubleSerializer.get.fromByteBuffer(calculatedSpeed)
+                checkin.venueName = StringSerializer.get.fromByteBuffer(tuple(7))
+                checkin.venueAddress = StringSerializer.get.fromByteBuffer(tuple(8))
+                checkin.venueSiteUrl = StringSerializer.get.fromByteBuffer(tuple(9))
+                checkin.venueId = StringSerializer.get.fromByteBuffer(tuple(10))
+                checkin.message = StringSerializer.get.fromByteBuffer(tuple(11))
+                checkin.serviceCheckinId = StringSerializer.get.fromByteBuffer(tuple(12))
+                checkin.notPublic = BooleanSerializer.get.fromByteBuffer(tuple(13))
+                checkin.clientId = StringSerializer.get.fromByteBuffer(tuple(14))
+                checkin.raw = StringSerializer.get.fromByteBuffer(tuple(15))
+                checkin.horizontalAccuracy = DoubleSerializer.get.fromByteBuffer(tuple(16))
+                checkin.verticalAccuracy = DoubleSerializer.get.fromByteBuffer(tuple(17))
+                checkin.batteryLevel = DoubleSerializer.get.fromByteBuffer(tuple(18))
+                checkin.course = DoubleSerializer.get.fromByteBuffer(tuple(19))
+                checkin.speed = DoubleSerializer.get.fromByteBuffer(tuple(20))
+                checkin.calculatedSpeed = DoubleSerializer.get.fromByteBuffer(tuple(21))
+                checkin.ip = StringSerializer.get.fromByteBuffer(tuple(22))
                 Some((checkin.id, checkin, checkin.ip != null, checkin.serviceType))
             }
         }
