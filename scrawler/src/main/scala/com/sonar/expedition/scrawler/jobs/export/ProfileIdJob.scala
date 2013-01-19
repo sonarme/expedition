0738874c48de8c619f8c9654457f6212228b5c8c
Ben Kempe <ben@k2medialabs.com>
2013-01-18 20:28:09 -0500

    stats

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/export/ProfileIdJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/export/ProfileIdJob.scala
@@ -3,7 +3,7 @@ package com.sonar.expedition.scrawler.jobs.export
 import com.sonar.expedition.scrawler.jobs.DefaultJob
 import com.twitter.scalding.{Tsv, SequenceFile, Args}
 import com.sonar.expedition.scrawler.util.Tuples
-import com.sonar.dossier.dto.ServiceProfileLink
+import com.sonar.dossier.dto.{ServiceType, ServiceProfileLink}
 
 class ProfileIdJob(args: Args) extends DefaultJob(args) {
     val in = args("in")
@@ -11,8 +11,15 @@ class ProfileIdJob(args: Args) extends DefaultJob(args) {
     val profileViewFile = SequenceFile(in + "_ProfileView", Tuples.ProfileIdDTO)
 
     // Combine profile pipes
-    val allProfiles = serviceProfileFile.read ++ profileViewFile.read
-    allProfiles.map('profileId -> 'profileIdStr) {
+    val allProfiles = (serviceProfileFile.read ++ profileViewFile.read).map('profileId -> 'profileId) {
         spl: ServiceProfileLink => spl.profileId
-    }.unique('profileIdStr).write(Tsv(in + "_ids"))
+    }
+    val numServiceType =
+        allProfiles.discard('profileDto).unique('profileId, 'serviceType).groupBy('serviceType) {
+            _.size.reducers(1)
+        }.map('serviceType -> 'statName) {
+            serviceType: ServiceType => "num_" + serviceType.name()
+        }.project('statName, 'size)
+    numServiceType.write(Tsv(in + "_stats2", ('statName, 'size)))
+
 }
