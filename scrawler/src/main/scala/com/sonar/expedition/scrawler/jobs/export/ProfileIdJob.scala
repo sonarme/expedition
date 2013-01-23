365ef35271f667be95a96612368af8c0dc18c2fe
Ben Kempe <ben@k2medialabs.com>
2013-01-23 12:36:20 -0500

    more test

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/export/ProfileIdJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/export/ProfileIdJob.scala
@@ -11,17 +11,26 @@ class ProfileIdJob(args: Args) extends DefaultJob(args) {
     val test = args.optional("test").map(_.toBoolean).getOrElse(false)
     val serviceProfileFile: Pipe =
         if (test)
-            IterableSource(Seq((ServiceProfileLink(ServiceType.facebook, "x"), ServiceProfileDTO(ServiceType.facebook, "x"), ServiceType.facebook)), Tuples.ProfileIdDTO).read
+
+            IterableSource(
+                (for (serviceType <- ServiceType.values().toIterable;
+                      name <- 1 to 100) yield
+                    (ServiceProfileLink(serviceType, name.toString), ServiceProfileDTO(serviceType, name.toString), serviceType))
+
+                , Tuples.ProfileIdDTO).read
         else SequenceFile(in + "_ServiceProfile", Tuples.ProfileIdDTO).read
     val profileViewFile: Pipe =
         if (test)
-            IterableSource(Seq((ServiceProfileLink(ServiceType.facebook, "x"), ServiceProfileDTO(ServiceType.facebook, "x"), ServiceType.facebook), (ServiceProfileLink(ServiceType.facebook, "y"), ServiceProfileDTO(ServiceType.facebook, "y"), ServiceType.facebook)), Tuples.ProfileIdDTO).read
+            IterableSource(
+                (for (serviceType <- ServiceType.values().toIterable;
+                      name <- 50 to 150) yield
+                    (ServiceProfileLink(serviceType, name.toString), ServiceProfileDTO(serviceType, name.toString), serviceType))
+                , Tuples.ProfileIdDTO).read
         else SequenceFile(in + "_ProfileView", Tuples.ProfileIdDTO).read
-    //serviceProfileFile.write(SequenceFile(in + "_ServiceProfile", Tuples.ProfileIdDTO))
-    //profileViewFile.write(SequenceFile(in + "_ProfileView", Tuples.ProfileIdDTO))
+
     // Combine profile pipes
     val allProfiles = (serviceProfileFile ++ profileViewFile).mapTo(('profileId, 'serviceType) ->('profileId, 'serviceType)) {
-        in: (ServiceProfileLink, ServiceType) => (in._1.profileId, in._2)
+        in: (ServiceProfileLink, ServiceType) => (in._1, in._2)
     }
 
     val numServiceType =
