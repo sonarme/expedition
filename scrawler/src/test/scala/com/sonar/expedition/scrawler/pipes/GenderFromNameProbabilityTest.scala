30f05d493eacbf59ed1f8d517f06939e4d7db9c3
Roger Chang <roger@sonar.me>
2013-01-24 14:29:48 -0500

    fix test

--- scrawler/src/test/scala/com/sonar/expedition/scrawler/pipes/GenderFromNameProbabilityTest.scala
+++ scrawler/src/test/scala/com/sonar/expedition/scrawler/pipes/GenderFromNameProbabilityTest.scala
@@ -2,28 +2,29 @@ package com.sonar.expedition.scrawler.pipes
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
+import com.sonar.dossier.dto.Gender
 
 class GenderFromNameProbabilityTest extends FlatSpec with ShouldMatchers {
     "gender classification" should "respond with high probability for a male name" in {
         val (gender, probability) = GenderFromNameProbability.gender("Paul")
-        assert(gender === "male")
+        assert(gender === Gender.male)
         assert(probability > 0.7)
     }
     it should "respond with high probability for an uppercase male name" in {
         val (gender, probability) = GenderFromNameProbability.gender("PAUL")
-        assert(gender === "male")
+        assert(gender === Gender.male)
         assert(probability > 0.7)
     }
 
     it should "respond with high probability for a female name" in {
         val (gender, probability) = GenderFromNameProbability.gender("Anna")
-        assert(gender === "female")
+        assert(gender === Gender.female)
         assert(probability > 0.7)
     }
 
     it should "respond with unknown" in {
         val (gender, probability) = GenderFromNameProbability.gender("xyz")
-        assert(gender === "unknown")
+        assert(gender === Gender.unknown)
         assert(probability >= 0)
     }
 }
