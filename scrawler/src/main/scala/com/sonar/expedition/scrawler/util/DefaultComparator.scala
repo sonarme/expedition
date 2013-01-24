fb26d9dd2525a86c4cc6722dd1d4f1723c171dda
Ben Kempe <ben@k2medialabs.com>
2013-01-24 11:41:09 -0500

    debug

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/util/DefaultComparator.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/util/DefaultComparator.scala
@@ -1,15 +1,20 @@
 package com.sonar.expedition.scrawler.util
 
 import com.twitter.scalding.IntegralComparator
+import org.slf4j.LoggerFactory
 
 class DefaultComparator extends IntegralComparator {
+    val LOG = LoggerFactory.getLogger(classOf[DefaultComparator])
 
-    override def hashCode(obj: AnyRef) =
+    override def hashCode(obj: AnyRef) = {
+        LOG.info("Found " + enumObj)
         if (obj == null) 0
         else if (obj.getClass.isEnum) {
             // fix enum hashing
             val enumObj = obj.asInstanceOf[Enum[_]]
+            LOG.info("Found enum " + enumObj)
             (obj.getClass, enumObj.name()).hashCode()
         }
         else super.hashCode(obj)
+    }
 }
