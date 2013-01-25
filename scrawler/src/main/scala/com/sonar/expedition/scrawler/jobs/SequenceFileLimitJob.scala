9060c78b1d6bcd67571c903480759d5a5d0028fb
Ben Kempe <ben@k2medialabs.com>
2013-01-25 14:24:03 -0500

    limit job

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/SequenceFileLimitJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/SequenceFileLimitJob.scala
@@ -1,11 +1,22 @@
 package com.sonar.expedition.scrawler.jobs
 
-import com.twitter.scalding.{SequenceFile, Args, Job}
+import com.twitter.scalding.{HadoopSchemeInstance, Args}
+import com.twitter.scalding.SequenceFile
+import cascading.scheme.local.{TextLine => CLTextLine, TextDelimited => CLTextDelimited}
+import cascading.scheme.hadoop.{TextLine => CHTextLine, TextDelimited => CHTextDelimited, SequenceFile => CHSequenceFile, WritableSequenceFile => CHWritableSequenceFile}
+import cascading.tuple.Fields
 
 class SequenceFileLimitJob(args: Args) extends DefaultJob(args) {
     val input = args("input")
     val output = args("output")
     val limit = args("limit").toInt
-    SequenceFile(input).limit(limit).write(SequenceFile(output))
+    SequenceFile(input).limit(limit).write(LimitedSequenceFile(output))
 }
 
+class LimitedSequenceFile(p: String, f: Fields = Fields.ALL, parts: Int = 1) extends SequenceFile(p, f) {
+    override def hdfsScheme = {
+        val scheme = HadoopSchemeInstance(new CHSequenceFile(fields))
+        scheme.setNumSinkParts(parts)
+        scheme
+    }
+}
