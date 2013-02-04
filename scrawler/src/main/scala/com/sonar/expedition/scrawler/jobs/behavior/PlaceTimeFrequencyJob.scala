f8cfca16f5649db491c36fdd815c53f5ac748f11
Ben Kempe <ben@k2medialabs.com>
2013-02-04 12:00:46 -0500

    moved timesegment back for now (because of kryo deserialization)

--- scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/behavior/PlaceTimeFrequencyJob.scala
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/behavior/PlaceTimeFrequencyJob.scala
@@ -3,25 +3,14 @@ package com.sonar.expedition.scrawler.jobs.behavior
 import com.twitter.scalding._
 import com.sonar.expedition.scrawler.pipes.CheckinGrouperFunction
 import com.sonar.expedition.scrawler.util.Tuples
-import java.util.Date
 import collection.JavaConversions._
-import org.scala_tools.time.Imports._
 import com.sonar.dossier.dto._
-import scala.Some
-import com.sonar.dossier.dto.LocationDTO
-import com.sonar.dossier.dto.GeodataDTO
 import com.sonar.dossier.dto.ServiceVenueDTO
 import com.sonar.dossier.dto.LocationDTO
 import com.twitter.scalding.Tsv
-import scala.Some
 import com.twitter.scalding.IterableSource
 import com.sonar.dossier.dto.GeodataDTO
-import cascading.pipe.joiner.LeftJoin
-import org.joda.time.DateMidnight
-import java.util
-import cascading.tuple.Fields
 import com.sonar.expedition.scrawler.jobs.DefaultJob
-import TimeSegment
 
 
 class PlaceTimeFrequencyJob(args: Args) extends DefaultJob(args) with CheckinGrouperFunction {
