27b1347e33f327acc65574555271a79b355214f7
Roger Chang <roger@sonar.me>
2013-02-06 12:15:03 -0500

    update good bid

--- analytics-common/src/test/scala/com/sonar/expedition/common/adx/search/rules/BidRequestRulesTest.scala
+++ analytics-common/src/test/scala/com/sonar/expedition/common/adx/search/rules/BidRequestRulesTest.scala
@@ -12,7 +12,7 @@ class BidRequestRulesTest extends FlatSpec {
     val nonMobileBid = BidRequest("1", List[Impression](), site = Site("123", name = "sonar", domain = "sonar.me", publisher = Publisher()))
     val bldomainBid = BidRequest("1", List[Impression](), app = App("123", name = "sonar", domain = "highlig.ht", publisher = Publisher()))
     val blcategoryBid = BidRequest("1", List[Impression](), app = App("123", name = "sonar", domain = "sonar.me", publisher = Publisher(cat = List[String]("IAB", "IAB2"))))
-    val goodBid = BidRequest("1", List[Impression](), app = App("123", name = "sonar", domain = "sonar.me", publisher = Publisher(cat = List[String]("IAB", "IAB3"))))
+    val goodBid = BidRequest("1", List[Impression](), app = App("123", name = "sonar", domain = "sonar.me", publisher = Publisher(cat = List[String]("IAB", "IAB3"))), device = Device(geo = Geo(lat = 40.738933f, lon = -73.988113f)))
     val badDimensionBid = BidRequest("1", List[Impression](Impression("a", banner = Banner(w = 10, h = 10))), app = App())
     val tooManyImpressionsBid = BidRequest("1", List[Impression](Impression("a", banner = Banner(w = 12, h = 12)), Impression("a", banner = Banner(w = 11, h = 11))), app = App())
 
