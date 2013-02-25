a81639534dd38e470b770c8744918975965daa0e
Roger Chang <roger@sonar.me>
2013-02-25 12:23:29 -0500

    timer for bidprocessor

--- analytics-common/src/main/scala/com/sonar/expedition/common/adx/search/service/BidProcessingService.scala
+++ analytics-common/src/main/scala/com/sonar/expedition/common/adx/search/service/BidProcessingService.scala
@@ -22,8 +22,9 @@ import collection.JavaConversions._
 import util.Random
 import org.openrtb.mobile.{Bid, SeatBid, BidResponse, BidRequest}
 import java.util.concurrent.atomic.AtomicInteger
+import com.yammer.metrics.scala.Instrumented
 
-object BidProcessingService extends TimeSegmentation {
+object BidProcessingService extends TimeSegmentation with Instrumented {
     val log = LoggerFactory.getLogger("application")
     val clickThroughRate = Map[BytesRef, CTR]() withDefaultValue (new CTR)
 
@@ -37,6 +38,8 @@ object BidProcessingService extends TimeSegmentation {
     lazy val searchService = multiDirectorySearchService("/media/ephemeral0/data")
     lazy val searchServiceCohorts = multiDirectorySearchService("/media/ephemeral0/dataCohorts")
     // TODO: shutdown
+
+    val requestTimer = metrics.timer("bidProcessor")
 
     val MaxAmountSpentHourly = 500 * 100
     val CurrentHourAmountSpent = new AtomicInteger(0)
@@ -67,88 +70,90 @@ object BidProcessingService extends TimeSegmentation {
 
 
     def processBidRequest(bidRequest: BidRequest, currentTime: Date = new Date): Option[BidResponse] = {
-        BidRequestRules.execute(bidRequest) match {
-            case Some(ruleViolation: String) => {
-                log.info("passing on bid. rule violation : " + ruleViolation)
-                None
-            }
-            case None => {
-                val bidPrice = if (random.nextFloat() <= epsilon) cpm
-                else {
-
-                    val Array(lat, lng) = bidRequest.getDevice.getLoc.split(" *, *").map(_.toDouble)
-
-                    // TODO: ugly copy&paste
-                    val timeSegment = hourSegment(lat, lng, currentTime)
-                    val geosector = GeoHash.withCharacterPrecision(lat, lng, 7).toBase32
-                    val timeWindow = JTHours.hoursBetween(new DateTime(0), new DateTime(currentTime)).getHours
-
-                    val more = searchService.moreLikeThis(Map(
-                        IndexField.Ip.toString -> bidRequest.getDevice.getIp,
-                        IndexField.Geosector.toString -> geosector,
-                        IndexField.GeosectorTimesegment.toString -> (geosector + ":" + timeSegment.toIndexableString),
-                        IndexField.GeosectorTimewindow.toString -> (geosector + ":" + timeWindow)
-                    ).filterNot(_._2 == null))
-
-                    val docTerms = (more.scoreDocs map {
-                        scoreDoc: ScoreDoc => scoreDoc -> searchServiceCohorts.terms(scoreDoc.doc, IndexField.SocialCohort)
-                    }).toMap[ScoreDoc, Iterable[BytesRef]]
-
-                    val termDocs = (for ((scoreDoc, terms) <- docTerms.toSeq; term <- terms) yield term -> scoreDoc).groupBy(_._1).toMap.mapValues(_.map(_._2))
-
-                    val docFreqs = docTerms.values.flatten.toSet.map {
-                        term: BytesRef => term -> searchServiceCohorts.docFreq(new Term(IndexField.SocialCohort.toString, term))
-                    }.toMap[BytesRef, Int]
-
-                    val termScores = termDocs.map {
-                        case (term, docs) =>
-                            term -> docs.map(_.score).sum / docFreqs(term)
-                    }
-                    val sumTermScores = termScores.values.sum
-                    val normalizedTermScores = termScores.mapValues(_ / sumTermScores)
-                    val averageClickThrough = normalizedTermScores.map {
-                        case (termEnum, normalizedScore) =>
-                            clickThroughRate(termEnum).get() * normalizedScore
-                    }.sum / normalizedTermScores.size
-
-
-                    /* val serviceIds = more.scoreDocs.map {
-                         case topDoc =>
-                             val doc = searchService.doc(topDoc.doc)
-                             doc.getField(IndexField.ServiceId.toString).stringValue()
-                     }
-                     log.info("doc serviceIds: " + serviceIds.mkString(", "))
-                     log.info("score : " + totalScore)*/
-                    assert(averageClickThrough <= 1f)
-                    averageClickThrough * cpm
+        requestTimer.time {
+            BidRequestRules.execute(bidRequest) match {
+                case Some(ruleViolation: String) => {
+                    log.info("passing on bid. rule violation : " + ruleViolation)
+                    None
                 }
-                val bidId = bidRequest.getId //for tracking and debugging. we can probably just use the bidRequest.id since we only handle one impression per bidRequest
-                val impId = bidRequest.getImpList.head.getImpid //we will always have one impression with a bid (based on the rules we specified in BidRequestRules)
-                val response = new BidResponse(bidId)
-                response.setBidid(bidId)
-                response.setCur("USD")
-                response.setUnits(0) // CPM
-                val bidPriceFormatted = "%.2f" format (bidPrice / 100)
-                response.setSeatbidList(Seq({
-                    val sb = new SeatBid
-                    sb.setBidList(Seq({
-                        val b = new Bid(impId, bidPriceFormatted)
-                        b.setAdid("") //ID that references the ad to be served if the bid wins.
-                        b.setNurl(notifyUrl) //Win notice URL.
-                        //b.setAdm("") //Actual XHTML ad markup.
-                        //b.setAdomain("") //Advertiser's primary or top-level domain for advertiser checking.
-                        //b.setIurl("") //Sample image URL (without cache busting) for content checking.
-                        //b.setCid("") //Campaign ID or similar that appears within the ad markup.
-                        //b.setCrid("") //Creative ID for reporting content issues or defects.
-                        //b.setAttrList(Seq())
-                        b
+                case None => {
+                    val bidPrice = if (random.nextFloat() <= epsilon) cpm
+                    else {
+
+                        val Array(lat, lng) = bidRequest.getDevice.getLoc.split(" *, *").map(_.toDouble)
+
+                        // TODO: ugly copy&paste
+                        val timeSegment = hourSegment(lat, lng, currentTime)
+                        val geosector = GeoHash.withCharacterPrecision(lat, lng, 7).toBase32
+                        val timeWindow = JTHours.hoursBetween(new DateTime(0), new DateTime(currentTime)).getHours
+
+                        val more = searchService.moreLikeThis(Map(
+                            IndexField.Ip.toString -> bidRequest.getDevice.getIp,
+                            IndexField.Geosector.toString -> geosector,
+                            IndexField.GeosectorTimesegment.toString -> (geosector + ":" + timeSegment.toIndexableString),
+                            IndexField.GeosectorTimewindow.toString -> (geosector + ":" + timeWindow)
+                        ).filterNot(_._2 == null))
+
+                        val docTerms = (more.scoreDocs map {
+                            scoreDoc: ScoreDoc => scoreDoc -> searchServiceCohorts.terms(scoreDoc.doc, IndexField.SocialCohort)
+                        }).toMap[ScoreDoc, Iterable[BytesRef]]
+
+                        val termDocs = (for ((scoreDoc, terms) <- docTerms.toSeq; term <- terms) yield term -> scoreDoc).groupBy(_._1).toMap.mapValues(_.map(_._2))
+
+                        val docFreqs = docTerms.values.flatten.toSet.map {
+                            term: BytesRef => term -> searchServiceCohorts.docFreq(new Term(IndexField.SocialCohort.toString, term))
+                        }.toMap[BytesRef, Int]
+
+                        val termScores = termDocs.map {
+                            case (term, docs) =>
+                                term -> docs.map(_.score).sum / docFreqs(term)
+                        }
+                        val sumTermScores = termScores.values.sum
+                        val normalizedTermScores = termScores.mapValues(_ / sumTermScores)
+                        val averageClickThrough = normalizedTermScores.map {
+                            case (termEnum, normalizedScore) =>
+                                clickThroughRate(termEnum).get() * normalizedScore
+                        }.sum / normalizedTermScores.size
+
+
+                        /* val serviceIds = more.scoreDocs.map {
+                             case topDoc =>
+                                 val doc = searchService.doc(topDoc.doc)
+                                 doc.getField(IndexField.ServiceId.toString).stringValue()
+                         }
+                         log.info("doc serviceIds: " + serviceIds.mkString(", "))
+                         log.info("score : " + totalScore)*/
+                        assert(averageClickThrough <= 1f)
+                        averageClickThrough * cpm
+                    }
+                    val bidId = bidRequest.getId //for tracking and debugging. we can probably just use the bidRequest.id since we only handle one impression per bidRequest
+                    val impId = bidRequest.getImpList.head.getImpid //we will always have one impression with a bid (based on the rules we specified in BidRequestRules)
+                    val response = new BidResponse(bidId)
+                    response.setBidid(bidId)
+                    response.setCur("USD")
+                    response.setUnits(0) // CPM
+                    val bidPriceFormatted = "%.2f" format (bidPrice / 100)
+                    response.setSeatbidList(Seq({
+                        val sb = new SeatBid
+                        sb.setBidList(Seq({
+                            val b = new Bid(impId, bidPriceFormatted)
+                            b.setAdid("") //ID that references the ad to be served if the bid wins.
+                            b.setNurl(notifyUrl) //Win notice URL.
+                            b.setAdm("") //Actual XHTML ad markup.
+                            b.setAdomain("") //Advertiser's primary or top-level domain for advertiser checking.
+                            //b.setIurl("") //Sample image URL (without cache busting) for content checking.
+                            //b.setCid("") //Campaign ID or similar that appears within the ad markup.
+                            //b.setCrid("") //Creative ID for reporting content issues or defects.
+                            //b.setAttrList(Seq())
+                            b
+                        }))
+                        sb.setGroup(0)
+                        sb
                     }))
-                    sb.setGroup(0)
-                    sb
-                }))
 
 
-                Option(response)
+                    Option(response)
+                }
             }
         }
     }
