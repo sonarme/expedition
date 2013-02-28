3d5fd8ef81b5ce915b787deba4e2ef4c1a3db032
Ben Kempe <ben@k2medialabs.com>
2013-02-28 18:11:17 -0500

    simple place inference

---
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/behavior/SimplePlaceInferenceJob.scala
@@ -0,0 +1,85 @@
+package com.sonar.expedition.scrawler.jobs.behavior
+
+import com.sonar.dossier.dto._
+import com.twitter.scalding._
+import com.sonar.expedition.scrawler.util.Tuples
+import com.sonar.dossier.{dto, Normalizers}
+import com.sonar.expedition.scrawler.checkins.CheckinInference
+import com.sonar.expedition.scrawler.util.CommonFunctions._
+import com.sonar.expedition.scrawler.util.CommonFunctions.Segment
+import com.twitter.scalding.IterableSource
+import com.sonar.dossier.dto.GeodataDTO
+import org.scala_tools.time.Imports._
+import com.sonar.expedition.scrawler.jobs.DefaultJob
+import com.sonar.expedition.common.segmentation.TimeSegmentation
+import ch.hsr.geohash.{WGS84Point, BoundingBox, GeoHash}
+import collection.JavaConversions._
+
+class SimplePlaceInferenceJob(args: Args) extends DefaultJob(args) with Normalizers with CheckinInference with TimeSegmentation {
+    val segments = Seq(1 -> 7, 7 -> 11, 11 -> 14, 14 -> 16, 16 -> 20, 20 -> 1) map {
+        case (fromHr, toHr) => Segment(from = fromHr, to = toHr, name = fromHr + "-" + toHr)
+    }
+    val test = args.optional("test").map(_.toBoolean).getOrElse(false)
+    val checkinSource = if (test) IterableSource(Seq(
+
+        dto.CheckinDTO(ServiceType.sonar,
+            "corner1",
+            GeodataDTO(40.745241, -73.982942),
+            DateTime.now,
+            "ben123",
+            None
+        ),
+        dto.CheckinDTO(ServiceType.sonar,
+            "corner2",
+            GeodataDTO(40.744575, -73.983028),
+            DateTime.now,
+            "ben123",
+            None
+        )
+
+    ).map(c => c.id -> c), Tuples.CheckinIdDTO)
+    else SequenceFile(args("checkinsIn"), Tuples.CheckinIdDTO)
+    val checkins = checkinSource.read
+    val bb = new BoundingBox(new WGS84Point(24.5210, -124.7625), new WGS84Point(49.3845, -66.9326))
+    val segmentedCheckins = checkins.flatMapTo(('checkinDto) ->('checkinId, 'spl, 'location, 'timeSegment, 'geosector)) {
+        dto: CheckinDTO =>
+            if (dto.serviceProfileId == null || dto.serviceType != ServiceType.sonar || !bb.contains(dto.geohash.getBoundingBoxCenterPoint)) Iterable.empty
+            else {
+                val ldt = localDateTime(dto.latitude, dto.longitude, dto.checkinTime.toDate)
+                val weekDay = isWeekDay(ldt)
+                val centerSector = smallSector(dto.latitude, dto.longitude)
+                // create tuples for each time segment
+                for (segment <- createSegments(ldt.toLocalTime.getHourOfDay, segments, Some((24, 0)));
+                     geosector <- centerSector.getAdjacent :+ centerSector) yield
+                    (dto.canonicalId, dto.profileId, dto.serviceVenue.location.geodata.canonicalLatLng, TimeSegment(weekDay, segment.name).toIndexableString, geosector.longValue())
+            }
+    }
+
+    val venues = if (test) IterableSource(Seq(
+        ("gg", ServiceVenueDTO(ServiceType.foursquare, "gg", "gg", location = LocationDTO(GeodataDTO(40.744916, -73.982599), "x"), category = Seq("coffee"))),
+        ("dd", ServiceVenueDTO(ServiceType.foursquare, "dd", "dd", location = LocationDTO(GeodataDTO(40.744835, -73.982706), "x"), category = Seq("coffee", "bagels"))),
+        ("hp24", ServiceVenueDTO(ServiceType.foursquare, "hp24", "hp24", location = LocationDTO(GeodataDTO(40.745144, -73.983006), "x"), category = Seq("hair")))
+    ), Tuples.VenueIdDTO)
+    else SequenceFile(args("venuesIn"), Tuples.VenueIdDTO)
+
+    val sectorizedVenues = venues.read.flatMapTo(('venueDto) ->('geosector, 'venueType)) {
+        dto: ServiceVenueDTO =>
+            if (dto.serviceType == ServiceType.foursquare)
+                dto.getCategory.map {
+                    category: String => (smallSector(dto.location.geodata.latitude, dto.location.geodata.longitude).longValue(), category)
+                }
+            else Iterable.empty
+    }
+    val placeInference = segmentedCheckins.joinWithSmaller(('geosector -> 'geosector), sectorizedVenues)
+            .groupBy('checkinId, 'spl, 'location, 'timeSegment) {
+        _.foldLeft('venueType -> 'placeInference)(Map.empty[String, Int]) {
+            (agg: Map[String, Int], venueType: String) =>
+                agg + ((venueType, agg.getOrElse(venueType, 0) + 1))
+
+        }
+    }
+    val placeInferenceOut = Tsv(args("placeInferenceOut"))
+    placeInference.write(placeInferenceOut)
+
+    def smallSector(lat: Double, lng: Double) = GeoHash.withBitPrecision(lat, lng, 8 * 5 - 2)
+}
