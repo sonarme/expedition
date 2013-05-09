28d3e54abbbb34760f016fd11e393f3bd778045f
Roger Chang <roger@sonar.me>
2013-05-09 17:29:53 -0400

    find geofence actions

---
+++ scrawler/src/main/scala/com/sonar/expedition/scrawler/jobs/FindGeofenceActionJob.scala
@@ -0,0 +1,125 @@
+package com.sonar.expedition.scrawler.jobs
+
+import com.twitter.scalding._
+import com.sonar.dossier.dto
+import com.sonar.dossier.dto._
+import org.scala_tools.time.Imports._
+import com.sonar.expedition.scrawler.util.{Haversine, Tuples}
+import collection.JavaConversions._
+import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import com.sonar.dossier.dto.ServiceVenueDTO
+import scala.Some
+import com.sonar.dossier.dto.LocationDTO
+import com.twitter.scalding.IterableSource
+import com.sonar.dossier.dto.GeodataDTO
+
+class FindGeofenceActionJob(args: Args) extends DefaultJob(args) {
+    val geofencesIn = args("geofences")
+    val test = args.optional("test").map(_.toBoolean).getOrElse(false)
+    val checkinsIn = args("checkinsIn")
+    val output = args("output")
+
+    val checkinSource = if (test) IterableSource(Seq(
+        dto.CheckinDTO(ServiceType.foursquare,
+            "walmart0",
+            GeodataDTO(42.101077409090805, -71.05758522727374),
+            DateTime.now,
+            "ben1234",
+            Some(ServiceVenueDTO(ServiceType.foursquare, "wm", "Walmart", location = LocationDTO(GeodataDTO(42.101077409090805, -71.05758522727374), "x")))
+        ),
+        dto.CheckinDTO(ServiceType.foursquare,
+            "test1",
+            GeodataDTO(40.7505800, -73.9935800),
+            DateTime.now,
+            "ben123",
+            Some(ServiceVenueDTO(ServiceType.foursquare, "chi", "Chipotle", location = LocationDTO(GeodataDTO(40.7505800, -73.9935800), "x")))
+        ),
+        dto.CheckinDTO(ServiceType.foursquare,
+            "test2",
+            GeodataDTO(40.749712, -73.993092),
+            DateTime.now,
+            "ben123",
+            Some(ServiceVenueDTO(ServiceType.foursquare, "gg", "G&G", location = LocationDTO(GeodataDTO(40.0, -74.0000012), "x")))
+        ),
+        dto.CheckinDTO(ServiceType.sonar,
+            "walmart1",
+            GeodataDTO(40.07763212, -74.1174186),
+            DateTime.now,
+            "ben123",
+            None
+        ),
+        dto.CheckinDTO(ServiceType.sonar,
+            "walmart1exit",
+            GeodataDTO(40.750183, -73.992513),
+            DateTime.now + 10.minutes,
+            "ben123",
+            None
+        ),
+        dto.CheckinDTO(ServiceType.sonar,
+            "test5",
+            GeodataDTO(40.750183, -73.992514),
+            DateTime.now + 30.minutes,
+            "ben123",
+            None
+        ),
+        dto.CheckinDTO(ServiceType.sonar,
+            "walmart2",
+            GeodataDTO(42.1010, -71.0575),
+            DateTime.now + 1.hour,
+            "ben123",
+            None
+        ),
+        dto.CheckinDTO(ServiceType.sonar,
+            "walmart2exit",
+            GeodataDTO(40.791979, -73.957214),
+            DateTime.now + 2.hours,
+            "ben123",
+            None
+        )
+    ).map(c => c.id -> c), Tuples.CheckinIdDTO)
+    else SequenceFile(checkinsIn, Tuples.CheckinIdDTO)
+
+    //for each sonar checkin, see if it is in the list of walmarts
+    val checkins = checkinSource
+        .filter('checkinDto) {checkinDto: CheckinDTO => checkinDto.serviceType == ServiceType.sonar}
+        .map('checkinDto -> ('sonarId, 'checkinTime)) { checkinDto: CheckinDTO => (checkinDto.serviceProfileId, checkinDto.checkinTime) }
+        .groupBy('sonarId) { _.toList[CheckinDTO]('checkinDto -> 'checkinDtoList).sortBy('checkinTime).reverse}
+        //split list into two...one for walmart checkins and another for anything else
+        .map('checkinDtoList -> ('targetedPongs, 'otherPongs)) {
+            checkinDtoList: List[CheckinDTO] => {
+                //find each walmart checkin and the checkin after that to use as the exit
+                checkinDtoList.partition {
+                    checkinDto: CheckinDTO => {
+                        val geofences = getGeofences(geofencesIn)
+                        geofences.find(wm =>Haversine.haversineInMeters(checkinDto.latitude, checkinDto.longitude, wm.get("latitude").asDouble(), wm.get("longitude").asDouble) <= 200) match {
+                            case Some(location) => checkinDto.serviceCheckinId = location.get("factual_id").textValue(); true
+                            case None => false
+                        }
+
+                    }
+                }
+            }
+        }
+        .discard('checkinDtoList)
+        .flatMapTo(('sonarId, 'targetedPongs, 'otherPongs) -> ('appId, 'platform, 'deviceId, 'geofenceId, 'lat, 'lng, 'entering, 'exiting)) {
+            in: (String, List[CheckinDTO], List[CheckinDTO]) => {
+                val (sonarId, targetedPongs, otherPongs) = in
+                //for each targetedPong...look for a corresponding otherpong which is the first pong after the targeted pong...this is a geofence exit action
+                val geofenceActions = targetedPongs.map(tp => (tp, otherPongs.find(tp.checkinTime < _.checkinTime).getOrElse(tp)))
+                val dtf = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss")
+                geofenceActions.map{ case(enter, exit) => ("sampler", "ios", sonarId, enter.serviceCheckinId, enter.latitude, enter.longitude, enter.checkinTime.withZone(DateTimeZone.UTC).toString(dtf), exit.checkinTime.withZone(DateTimeZone.UTC).toString(dtf))}
+            }
+        }
+
+    //group the checkins by user
+    checkins.write(Csv(output))
+
+
+    private def getGeofences(file: String) = {
+        val source = io.Source.fromFile(file)
+        val locations = source.mkString
+        source.close()
+        val om = new ObjectMapper
+        om.readValue(locations, classOf[JsonNode]).fields().flatMap(_.getValue).toList
+    }
+}
